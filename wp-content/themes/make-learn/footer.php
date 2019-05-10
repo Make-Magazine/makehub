cc83ac0a0db919867a6e841c4df7537796524a8b
Rio Roth-Barrerio <rio@makermedia.com>
2019-05-10 13:37:37 -0700

    remove usersnap

--- wp-content/themes/make-learn/footer.php
+++ wp-content/themes/make-learn/footer.php
@@ -37,24 +37,6 @@ genesis_markup(
   <div class="clearfix"></div>
 </div>
 
-<!-- Usersnap feedback and bug tracker -->
-<script>
-  	window.onUsersnapLoad = function(api) {
-	api.on('open', function(event) {
-		/*event.api.setValue('customData', JSON.stringify({
-		  Company: 'MakerMedia',
-		  User: 'Rio'
-		}));*/
-	});
-	api.init();
-	window.Usersnap = api;
-  	}
-  	var script = document.createElement('script');
-	script.async = 1;
-	script.src = 'https://api.usersnap.com/load/ab520b96-6180-4bf7-bd3e-11786a368bce.js?onload=onUsersnapLoad';
-	document.getElementsByTagName('head')[0].appendChild(script);
-</script>
-
 <?php if(is_front_page()){ ?>
 	<script async>(function(s,u,m,o,j,v){j=u.createElement(m);v=u.getElementsByTagName(m)[0];j.async=1;j.src=o;j.dataset.sumoSiteId='6d09a6b9f0dfb10b9fbcebf5702dcbe99280f2a4fedcc1e8d64c47156312fd1a';v.parentNode.insertBefore(j,v)})(window,document,'script','//load.sumo.com/');</script>
 <?php } ?>
