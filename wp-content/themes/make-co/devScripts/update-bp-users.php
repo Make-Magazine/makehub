0f241e3fd3106874af00ac4c09feab68772e1abe
Rio Roth-Barrerio <rio@makermedia.com>
2019-07-30 10:40:27 -0700

    try adding bp-custom functions for countries

--- wp-content/themes/make-co/devScripts/update-bp-users.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  $members =  get_users( 'blog_id=1&fields=ID' );
-  
-  foreach ( $members as $user_id ) {
-	     error_log($user_id);
-        bp_update_user_last_activity( $user_id, bp_core_current_time() );
-  }
-
