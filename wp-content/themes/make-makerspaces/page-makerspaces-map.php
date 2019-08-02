5ecbc2f4b21ea1899f8ac03403f03097272d0479
Alicia Williams <alicia@makermedia.com>
2019-08-02 11:44:01 -0600

    moving functions out of the page template and into their own functions file

--- wp-content/themes/make-makerspaces/page-makerspaces-map.php
+++ wp-content/themes/make-makerspaces/page-makerspaces-map.php
@@ -94,31 +94,6 @@ get_header(); ?>
          $rss_items = $rss -> get_items(0, $maxitems);
       endif;
 
-      //grabs our post thumbnail image
-      function get_first_image_url($html) {
-         if (preg_match('/<img.+?src="(.+?)"/', $html, $matches)) {
-            return $matches[1];
-         }
-      }
-      
-      // get just the text
-      function get_summary($html) {
-         $summary = preg_replace('/<a[^>]*>([\s\S]*?)<\/a[^>]*>/', '', $html);
-         $summary = strip_tags(str_replace('The post appeared first on .', '', $summary));
-         return $summary;
-      }
-
-      //shortens description
-      function shorten($string, $length) {
-         $suffix = '&hellip;';
-         $short_desc = trim(str_replace(array("\r", "\n", "\t"), ' ', strip_tags($string)));
-         $desc = trim(substr($short_desc, 0, $length));
-         $lastchar = substr($desc, -1, 1);
-         if ($lastchar == '.' || $lastchar == '!' || $lastchar == '?')
-         $suffix = '';
-         $desc .= $suffix;
-         return $desc;
-      }
       ?>
       <?php
       if ($maxitems == 0) echo '<li>No items.</li>';
