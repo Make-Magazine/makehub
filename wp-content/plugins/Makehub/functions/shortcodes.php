7693c4d493e457e818be0a2023f0259e28ea473d
Alicia Williams <alicia@makermedia.com>
2019-08-28 14:52:00 -0600

    adding makehub plugin. this plugin will include all of the common functions used across the child themes in the makehub

---
+++ wp-content/plugins/Makehub/functions/shortcodes.php
@@ -0,0 +1,54 @@
+<?php
+
+// [make_rss title="Makerspace", feed="https://makezine.com/tag/makerspaces/feed/", moreLink="http://makezine.com/tag/makerspaces/", number=4]
+function make_rss_func($atts) {
+    $a = shortcode_atts(array(
+        'title' => '',
+        'feed' => 'https://makezine.com/feed/',
+        'moreLink' => "",
+        'number' => 4
+            ), $atts);
+    $return = '    
+    <div class="container makerspace-news">
+        <h2>' . $a['title'] . ' News from <img class="logo" src="https://make.co/wp-content/themes/memberships/img/make_logo.svg" /> magazine</h2>
+        <div class="row posts-feeds-wrapper">';
+
+
+    $rss = fetch_feed($a['feed']);
+    if (!is_wp_error($rss)) {
+        $maxitems = $rss->get_item_quantity($a['number']); //gets latest x items, this can be changed to suit your requirements
+        $rss_items = $rss->get_items(0, $maxitems);
+    }
+
+    if ($maxitems == 0) {
+        $return .= '<li>No items.</li>';
+    } else {
+        foreach ($rss_items as $item) {
+            $return .= '
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <div class="post-feed">
+                            <a class="full-link" href="' . esc_url($item->get_permalink()) . '" target="_blank">
+                                <div class="title">
+                                    <img src="' . get_first_image_url($item->get_content()) . '" alt="' . $a['title'] . ' post featured image">
+                                    <p class="p-title">' . esc_html($item->get_title()) . '</p>
+                                    <p>' . shorten(get_summary($item->get_content()), 100) . '</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>';
+        }
+    }
+    if ($a['moreLink'] != '') {
+        $return .= '
+                <div class="col-xs-12">
+                    <a class="btn universal-btn btn-more-articles" href="' . $a['moreLink'] . '" target="_blank">See more articles</a>
+                </div>';
+    }
+    $return .= '    
+        </div>
+
+    </div>';
+    return $return;
+}
+
+add_shortcode('make_rss', 'make_rss_func');
---
+++ wp-content/plugins/Makehub/makehub.php
@@ -0,0 +1,31 @@
+<?php
+/*
+   Plugin Name: Makehub Functions
+   Plugin URI: makermedia.com
+   Description: This plugin adds common makehub functionality used across the Genesis child themes for makehub
+   Version: 1.0
+   Author: Alicia Williams
+   License: GPL2
+   */
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+// Include all function files in the functions directory:
+foreach (glob(plugin_dir_path( __FILE__ ) . '/functions/*.php') as $file) {
+   include_once $file;
+}
+
+add_action( 'wp_enqueue_scripts', 'makehub_enqueue_scripts' );
+function makehub_enqueue_scripts(){
+    //add scripts here
+}
+
+add_action( 'wp_enqueue_scripts', 'makehub_enqueue_styles');
+function makehub_enqueue_styles() {
+    $my_version = '1.0.0';
+    wp_enqueue_style('makehub-style', plugins_url( '/css/style.min.css', __FILE__ ), array(), $my_version );
+}
