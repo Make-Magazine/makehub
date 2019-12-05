7daba981ea98fbce3e5cad85df59801c7ce1191b
Alicia Williams <alicia@make.co>
2019-12-05 14:50:48 -0700

    removing old auth0 libraries and functions. this will all be handled with the auth0 plugin going forward

--- wp-content/themes/make-co/js/auth0-variables.js
+++
@@ -1,8 +0,0 @@
-var AUTH0_CLIENT_ID    = 'DRzbPDCo8kW5sDQ0e5Ynl0yaCAS2iR6e' 
-var AUTH0_DOMAIN       = 'makermedia.auth0.com';
-
-if (typeof templateUrl === 'undefined') {
-  var templateUrl = window.location.origin;
-}
-var AUTH0_CALLBACK_URL = templateUrl + "/authenticate-redirect/";
-var AUTH0_REDIRECT_URL = templateUrl;
