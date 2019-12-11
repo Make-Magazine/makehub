2b9417103dbc0f9def9b6befc871818e49970821
Alicia Williams <alicia@make.co>
2019-12-11 12:57:04 -0700

    don't ask

--- wp-content/themes/make-makerspaces/functions/gravityforms_notifications.php
+++
@@ -1,7 +0,0 @@
-<?php
-//add new Notification event of manual to allow users to set up manual notifications
-add_filter( 'gform_notification_events', 'add_event' );
-function add_event( $notification_events ) {
-    $notification_events['manual']                = __( 'Send Manually', 'gravityforms' );
-    return $notification_events;
-}
