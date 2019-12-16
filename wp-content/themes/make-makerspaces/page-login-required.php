5a438ef003bcd1799b483001cfa3a192ccfa274f
Rio Roth-Barrerio <rio@makermedia.com>
2019-12-16 14:28:00 -0800

    no content showing in direct port of login-required page template

--- wp-content/themes/make-makerspaces/page-login-required.php
+++ wp-content/themes/make-makerspaces/page-login-required.php
@@ -23,17 +23,18 @@ get_header(); ?>
 
       <div class="col-xs-12">
 
-      	<?php while ( have_posts() ) : the_post(); ?>
-
-      		<?php get_template_part( 'content', 'page' ); ?>
-
-      		<?php
-      			// If comments are open or we have at least one comment, load up the comment template
-      			if ( comments_open() || '0' != get_comments_number() )
-      				comments_template();
-      		?>
-
-      	<?php endwhile; // end of the loop. ?>
+      	<?php 
+			if (have_posts()) {
+				 while (have_posts()) {
+					  the_post();
+					  ?>
+			  <div class="container">
+					<?php the_content(); ?>
+			  </div> 
+				 <?php 
+				  } // end while
+			} // end if
+			?>
 
       </div>
 
