95f78385be24704c4030f2c7f2a706b361d08b2d
Rio Roth-Barrerio <rio@makermedia.com>
2019-12-17 12:52:21 -0800

    missing content templates

---
+++ wp-content/themes/make-makerspaces/content-single.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package _makerspaces
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header>
+		<h1 class="page-title"><?php the_title(); ?></h1>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<div class="entry-content-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</div>
+		<?php the_content(); ?>
+		<?php _makerspaces_link_pages(); ?>
+	</div><!-- .entry-content -->
+   
+</article><!-- #post-## -->
