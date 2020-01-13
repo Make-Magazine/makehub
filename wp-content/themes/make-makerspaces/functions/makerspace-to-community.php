ea85ecab8284669431e78e11739a31e55dae4e32
Rio Roth-Barrerio <rio@makermedia.com>
2020-01-13 13:04:15 -0800

    remove some unnecessary lines of code

--- wp-content/themes/make-makerspaces/functions/makerspace-to-community.php
+++ wp-content/themes/make-makerspaces/functions/makerspace-to-community.php
@@ -90,7 +90,6 @@ function makerspace_to_community($entry, $form) {
         $destination_bpfull = $image_dir . '/' . $current_time . '-bpfull.jpg';
         $destination_bpthumb = $image_dir . '/' . $current_time . '-bpthumb.jpg';
         
-        $usermeta['avatar'] = str_replace(' ', '%20', $bpmeta['avatar']);
         $bpfull = $bpthumb = wp_get_image_editor($bpmeta['avatar']);
         
         // Handle 404 avatar url
@@ -99,7 +98,6 @@ function makerspace_to_community($entry, $form) {
             $bpfull->save($destination_bpfull);
             $bpthumb->resize(50, 50, true);
             $bpthumb->save($destination_bpthumb);
-			   $bpmeta['avatar'] = $destination_bpfull;
         }        
     }
 
