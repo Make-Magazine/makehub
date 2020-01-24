495e56027b7329470bbd643b350f24955d07cc4f
Rio Roth-Barrerio <rio@makermedia.com>
2020-01-24 11:50:53 -0800

    update gravity view, yoast and auth0 plugins

--- wp-content/plugins/auth0/lib/WP_Auth0_LoginManager.php
+++ wp-content/plugins/auth0/lib/WP_Auth0_LoginManager.php
@@ -569,18 +569,15 @@ class WP_Auth0_LoginManager {
 	 * @return array
 	 */
 	public static function get_authorize_params( $connection = null, $redirect_to = null ) {
-		$params       = array();
-		$options      = WP_Auth0_Options::Instance();
-		$lock_options = new WP_Auth0_Lock10_Options();
-		$is_implicit  = (bool) $options->get( 'auth0_implicit_workflow', false );
-		$nonce        = WP_Auth0_Nonce_Handler::get_instance()->get_unique();
+		$params      = array();
+		$options     = WP_Auth0_Options::Instance();
+		$is_implicit = (bool) $options->get( 'auth0_implicit_workflow', false );
+		$nonce       = WP_Auth0_Nonce_Handler::get_instance()->get_unique();
 
 		$params['client_id']     = $options->get( 'client_id' );
 		$params['scope']         = self::get_userinfo_scope( 'authorize_url' );
 		$params['response_type'] = $is_implicit ? 'id_token' : 'code';
-		$params['redirect_uri']  = $is_implicit
-			? $lock_options->get_implicit_callback_url()
-			: $options->get_wp_auth0_url();
+		$params['redirect_uri']  = $options->get_wp_auth0_url( null, $is_implicit );
 
 		if ( $is_implicit ) {
 			$params['nonce']         = $nonce;
