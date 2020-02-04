b12b1379e5f006173d0598d141b07460a7b0c141
Rio Roth-Barrerio <rio@makermedia.com>
2020-02-04 13:09:13 -0800

    try the same function on makerspaces and learn

--- wp-content/themes/make-learn/functions.php
+++ wp-content/themes/make-learn/functions.php
@@ -148,6 +148,14 @@ function make_learn_enqueue_scripts_styles() {
 // remove the subtheme level style.css to use it as a version
 remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
 
+add_filter('show_admin_bar', 'remove_admin_bar', PHP_INT_MAX);
+// if you're not an admin, don't show the admin bar
+function remove_admin_bar(){
+    if (current_user_can('administrator')) {
+        return true;
+    }
+    return false;
+}
 
 /**
  * Defines responsive menu settings.
--- wp-content/themes/make-makerspaces/functions.php
+++ wp-content/themes/make-makerspaces/functions.php
@@ -160,8 +160,7 @@ function make_makerspaces_enqueue_scripts_styles() {
 // remove the subtheme level style.css to use it as a version
 remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
 
-// remove the subtheme level style.css to use it as a version
-remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
+add_filter('show_admin_bar', 'remove_admin_bar', PHP_INT_MAX);
 // if you're not an admin, don't show the admin bar
 function remove_admin_bar(){
     if (current_user_can('administrator')) {
