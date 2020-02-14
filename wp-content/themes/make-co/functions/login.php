80164a42732d8a4e96d3e9cfb42dd5bd49481be7
Rio Roth-Barrerio <rio@makermedia.com>
2020-02-13 16:01:56 -0800

    revert login styles for wp-login

--- wp-content/themes/make-co/functions/login.php
+++ wp-content/themes/make-co/functions/login.php
@@ -1,5 +1,7 @@
+<?php
+
 function custom_login() {
-    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/functions/assets/login-style.css' );
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/functions/assets/login-style.css', array('login') );
     wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/functions/assets/login-style.js' );
 }
 add_action( 'login_enqueue_scripts', 'custom_login' );
