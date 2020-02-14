a2438704cecf839afe4f08a487b1ee08e0346b05
Rio Roth-Barrerio <rio@makermedia.com>
2020-02-14 09:46:52 -0800

    untangling mess of auth0 vs non-auth0 login styling

--- wp-content/themes/make-co/functions/login.php
+++ wp-content/themes/make-co/functions/login.php
@@ -1,7 +1,11 @@
 <?php
 
+/* STYLING FOR AUTH0 Login
+
 function custom_login() {
     wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/functions/assets/login-style.css', array('login') );
     wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/functions/assets/login-style.js' );
 }
 add_action( 'login_enqueue_scripts', 'custom_login' );
+
+*/
