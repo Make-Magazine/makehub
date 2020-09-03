90c61b9372c71115949ea5fc5be342d1cdc77eae
Alicia Williams <alicia@make.co>
2020-09-03 09:20:21 -0600

    non change

--- wp-content/themes/make-campus/functions.php
+++ wp-content/themes/make-campus/functions.php
@@ -81,16 +81,16 @@ function genesis_child_gutenberg_support() { // phpcs:ignore WordPress.NamingCon
 function set_universal_constants() {
     // Assume that we're in prod; only change if we are definitively in another
     $context = stream_context_create(array());
-	$host = $_SERVER['HTTP_HOST'];
+    $host = $_SERVER['HTTP_HOST'];
     // legacy staging environments
     if (strpos($host, '.stagemakehub.wpengine.com') > -1 || strpos($host, '.devmakehub.wpengine.com') > -1) {
         $context = stream_context_create(array(
-                    'ssl' => array(
-                        'verify_peer'      => false,
-                        'verify_peer_name' => false,
-                        ), 
-                    )
-                );
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ),
+                )
+        );
     }
     // Set the important bits as CONSTANTS that can easily be used elsewhere
     define('STREAM_CONTEXT', $context);
@@ -106,29 +106,29 @@ add_action('wp_enqueue_scripts', 'make_campus_enqueue_scripts', 0);
  * @since 1.0.0
  */
 function make_campus_enqueue_scripts() {
-	$my_theme = wp_get_theme();
-	$my_version = $my_theme->get('Version');
-	$suffix = ( defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ) ? '' : '.min';
-	wp_enqueue_script(
-		'make-campus-responsive-menu',
-		get_stylesheet_directory_uri() . "/js/site/responsive-menus{$suffix}.js",
-		array('jquery'),
-		$my_version,
-		true
-	);
+    $my_theme = wp_get_theme();
+    $my_version = $my_theme->get('Version');
+    $suffix = ( defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ) ? '' : '.min';
+    wp_enqueue_script(
+            'make-campus-responsive-menu',
+            get_stylesheet_directory_uri() . "/js/site/responsive-menus{$suffix}.js",
+            array('jquery'),
+            $my_version,
+            true
+    );
 
-	wp_localize_script(
-		'make-campus-responsive-menu',
-		'genesis_responsive_menu',
-		make_campus_responsive_menu_settings()
-	);
+    wp_localize_script(
+            'make-campus-responsive-menu',
+            'genesis_responsive_menu',
+            make_campus_responsive_menu_settings()
+    );
 
-	wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), '', true);
-	wp_enqueue_script('fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.6/js/jquery.fancybox.min.js', array('jquery'), '', true);
+    wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), '', true);
+    wp_enqueue_script('fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.6/js/jquery.fancybox.min.js', array('jquery'), '', true);
 
-	wp_enqueue_script('fontawesome5-js', 'https://kit.fontawesome.com/7c927d1b5e.js', array(), '', true ); 
-	wp_enqueue_script('universal', content_url() . '/universal-assets/v1/js/min/universal.min.js', array(), $my_version, true);
-	wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/js/min/scripts.min.js', array('jquery'), $my_version, true);
+    wp_enqueue_script('fontawesome5-js', 'https://kit.fontawesome.com/7c927d1b5e.js', array(), '', true);
+    wp_enqueue_script('universal', content_url() . '/universal-assets/v1/js/min/universal.min.js', array(), $my_version, true);
+    wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/js/min/scripts.min.js', array('jquery'), $my_version, true);
 
 
     wp_localize_script('make-campus', 'ajax_object',
@@ -140,7 +140,6 @@ function make_campus_enqueue_scripts() {
                 'wp_user_nicename' => wp_get_current_user()->user_nicename
             )
     );
-
 }
 
 add_action('wp_enqueue_scripts', 'make_campus_enqueue_styles');
@@ -181,15 +180,15 @@ function make_campus_enqueue_styles() {
 remove_action('genesis_meta', 'genesis_load_stylesheet');
 
 add_filter('show_admin_bar', 'remove_admin_bar', PHP_INT_MAX);
+
 // if you're not an admin, don't show the admin bar
-function remove_admin_bar(){
+function remove_admin_bar() {
     if (current_user_can('administrator')) {
         return true;
     }
-    
+
     return false;
 }
-
 
 /**
  * Defines responsive menu settings.
@@ -296,17 +295,18 @@ add_action('genesis_footer', 'genesis_do_subnav', 10);
 
 add_filter('genesis_author_box_gravatar_size', 'make_campus_author_box_gravatar');
 /* Modifies size of the Gravatar in the author box. */
+
 function make_campus_author_box_gravatar($size) {
     return 90;
 }
 
 add_filter('genesis_comment_list_args', 'make_campus_comments_gravatar');
 /* Modifies size of the Gravatar in the entry comments. */
+
 function make_campus_comments_gravatar($args) {
     $args['avatar_size'] = 60;
     return $args;
 }
-
 
 // Function to change email address from wordpress to webmaster
 function wpb_sender_email($original_email_address) {
@@ -349,156 +349,159 @@ add_action('login_footer', function() {
 require_once( ABSPATH . 'wp-content/plugins/event-tickets/src/Tribe/Tickets.php');
 
 // All Event fields that aren't standard have to be mapped manually (_1 is the form id) had to remove it from the end of the action because the form name was different on stage :(
-add_action( 'gform_advancedpostcreation_post_after_creation', 'update_event_information', 10, 4 );
-function update_event_information( $post_id, $feed, $entry, $form ){
+add_action('gform_advancedpostcreation_post_after_creation', 'update_event_information', 10, 4);
+
+function update_event_information($post_id, $feed, $entry, $form) {
     //update the All Day setting
     $all_day = $entry['3.1'];
-    if ( $all_day == 'All Day' ){
-        update_post_meta( $post_id, '_EventAllDay', 'yes');
+    if ($all_day == 'All Day') {
+        update_post_meta($post_id, '_EventAllDay', 'yes');
     }
     $start_date = $entry['4'];
-    if ( $start_date ){
-        update_post_meta( $post_id, '_EventStartDate', $start_date );
+    if ($start_date) {
+        update_post_meta($post_id, '_EventStartDate', $start_date);
     }
-	$start_time = $entry['5'];
-    if ( $start_time ){
-        update_post_meta( $post_id, '_EventStartTime', $start_time );
+    $start_time = $entry['5'];
+    if ($start_time) {
+        update_post_meta($post_id, '_EventStartTime', $start_time);
     }
-	$end_date = $entry['6'];
-    if ( $end_date ){
-        update_post_meta( $post_id, '_EventEndDate', $end_date );
+    $end_date = $entry['6'];
+    if ($end_date) {
+        update_post_meta($post_id, '_EventEndDate', $end_date);
     }
-	$end_time = $entry['7'];
-    if ( $end_time ){
-        update_post_meta( $post_id, '_EventEndTime', $end_time );
+    $end_time = $entry['7'];
+    if ($end_time) {
+        update_post_meta($post_id, '_EventEndTime', $end_time);
     }
-	
-	// Update Audience Checkbox
-        /*
-    $field = GFAPI::get_field( $form, 73 );
-    if ( $field->type == 'checkbox' ) {
-        // Get a comma separated list of checkboxes checked
-        $checked = $field->get_value_export( $entry );
-        // Convert to array.
-        $values = explode( ', ', $checked );
-        // Serialize the data.
-        $value = maybe_serialize( $values );
-    }
-	//error_log(print_r($checked, TRUE));
-	//error_log(print_r($value, TRUE));
-	// NEITHER OF THESE WORK WHY OH WHY
-	update_post_meta( $post_id, 'field_5f35a5f833a04', $value );
-	update_field('field_5f35a5f833a04', array($checked), $post_id);*/
 
-	// create ticket for event // CHANGE TO WOOCOMMERCE AFTER PURCHASING EVENTS PLUS PLUGIN
-	$api = Tribe__Tickets__Commerce__PayPal__Main::get_instance();
-	$ticket = new Tribe__Tickets__Ticket_Object();
-	$ticket->name = $entry['40'];
-	$ticket->description = $entry['42'];;
-	$ticket->price = $entry['37'];
-	$ticket->capacity = $entry['43'];
-	$ticket->start_date = $entry['45'];
-	$ticket->start_time = $entry['46'];
-	$ticket->end_date = $entry['47'];
-	$ticket->end_time = $entry['48'];
-    
-	// Save the ticket
-	$ticket->ID = $api->save_ticket($post_id, $ticket, array(
-		'ticket_name' => $ticket->name,
-		'ticket_price' => $ticket->price,
-		'ticket_description' => $ticket->description,
-		'start_date' => $ticket->start_date,
-		'start_time' => $ticket->start_time,
-		'end_date' => $ticket->end_date,
-		'end_time' => $ticket->end_time,
-		'capacity' => $ticket->capacity,
-	));
-	/*
-	$tickets_handler = tribe( 'tickets.handler' );
-	$event_stock = new Tribe__Tickets__Global_Stock( $post_id );
-	$tickets_handler->remove_hooks();
-	// We need to update event post meta - if we've set a global stock
-	$event_stock->enable();
-	$event_stock->set_stock_level( $ticket->capacity, true );
-	update_post_meta( $post_id, $tickets_handler->key_capacity, $ticket->capacity );
-	update_post_meta( $post_id, $event_stock::GLOBAL_STOCK_ENABLED, 1 );
-	$tickets_handler->add_hooks();
-	error_log(print_r($data, TRUE));
-	error_log(print_r($ticket, TRUE));
-	error_log(print_r($tickets_handler, TRUE));
-	error_log(print_r($event_stock, TRUE));
-	*/
+    // Update Audience Checkbox
+    /*
+      $field = GFAPI::get_field( $form, 73 );
+      if ( $field->type == 'checkbox' ) {
+      // Get a comma separated list of checkboxes checked
+      $checked = $field->get_value_export( $entry );
+      // Convert to array.
+      $values = explode( ', ', $checked );
+      // Serialize the data.
+      $value = maybe_serialize( $values );
+      }
+      //error_log(print_r($checked, TRUE));
+      //error_log(print_r($value, TRUE));
+      // NEITHER OF THESE WORK WHY OH WHY
+      update_post_meta( $post_id, 'field_5f35a5f833a04', $value );
+      update_field('field_5f35a5f833a04', array($checked), $post_id); */
 
+    // create ticket for event // CHANGE TO WOOCOMMERCE AFTER PURCHASING EVENTS PLUS PLUGIN
+    $api = Tribe__Tickets__Commerce__PayPal__Main::get_instance();
+    $ticket = new Tribe__Tickets__Ticket_Object();
+    $ticket->name = $entry['40'];
+    $ticket->description = $entry['42'];
+    ;
+    $ticket->price = $entry['37'];
+    $ticket->capacity = $entry['43'];
+    $ticket->start_date = $entry['45'];
+    $ticket->start_time = $entry['46'];
+    $ticket->end_date = $entry['47'];
+    $ticket->end_time = $entry['48'];
+
+    // Save the ticket
+    $ticket->ID = $api->save_ticket($post_id, $ticket, array(
+        'ticket_name' => $ticket->name,
+        'ticket_price' => $ticket->price,
+        'ticket_description' => $ticket->description,
+        'start_date' => $ticket->start_date,
+        'start_time' => $ticket->start_time,
+        'end_date' => $ticket->end_date,
+        'end_time' => $ticket->end_time,
+        'capacity' => $ticket->capacity,
+    ));
+    /*
+      $tickets_handler = tribe( 'tickets.handler' );
+      $event_stock = new Tribe__Tickets__Global_Stock( $post_id );
+      $tickets_handler->remove_hooks();
+      // We need to update event post meta - if we've set a global stock
+      $event_stock->enable();
+      $event_stock->set_stock_level( $ticket->capacity, true );
+      update_post_meta( $post_id, $tickets_handler->key_capacity, $ticket->capacity );
+      update_post_meta( $post_id, $event_stock::GLOBAL_STOCK_ENABLED, 1 );
+      $tickets_handler->add_hooks();
+      error_log(print_r($data, TRUE));
+      error_log(print_r($ticket, TRUE));
+      error_log(print_r($tickets_handler, TRUE));
+      error_log(print_r($event_stock, TRUE));
+     */
 }
 
 // After the gravity view is updated, we want to update the created post associated with it. 
 // SO FAR, THIS IS UPDATING THE TITLE, CONTENT, FEATURED IMAGE, AND TEXT ACF FIELDS... needs work for taxonomies
-add_action( 'gravityview/edit_entry/after_update', 'gravityview_event_update', 10, 4 );
-function gravityview_event_update( $form, $entry_id, $entry_object ) {
-	$post_obj = gform_get_meta($entry_id, "gravityformsadvancedpostcreation_post_id");
-	$post_id = $post_obj[0]["post_id"];
-	$post_data = array(
-		  'ID'           => $post_id,
-		  'post_title'   => gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Event Title"),
-		  'post_content' => gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Describe What You Do"),
-		  'post_category' => gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Experience Type"),
-		  //'tags_input' => gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Category Tags")
-	);
-	wp_update_post($post_data);
-	//error_log(print_r(gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Experience Type"), TRUE));
-	//error_log(print_r(gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Category Tags"), TRUE));
-	//error_log("Featured Image: " . print_r(gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Featured Image")), TRUE);
-	require_once(ABSPATH . 'wp-admin/includes/media.php');
-	require_once(ABSPATH . 'wp-admin/includes/file.php');
-	require_once(ABSPATH . 'wp-admin/includes/image.php');
-	$media = media_sideload_image(gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Featured Image"), $post_id);
-	if(!empty($media) && !is_wp_error($media)){
-		$args = array(
-			'post_type' => 'attachment',
-			'posts_per_page' => -1,
-			'post_status' => 'any',
-			'post_parent' => $post_id
-		);
-		$attachments = get_posts($args);
-		if(isset($attachments) && is_array($attachments)){
-			foreach($attachments as $attachment){
-				$image = wp_get_attachment_image_src($attachment->ID, 'full');
-				// determine if in the $media image we created, the string of the URL exists
-				if(strpos($media, $image[0]) !== false){
-					// if so, we found our image. set it as thumbnail
-					set_post_thumbnail($post_id, $attachment->ID);
-					break;
-				}
-			}
-		}
-	}
-	// Not sure how else to update all the fields but to mention the by name
-	update_field("about", gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "About You"), $post_id);
-	update_field("location", gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Location"), $post_id);
-	update_field("materials", gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Experience Materials"), $post_id);
-}
-// rather than use potentially changing field ids, look up by label
-function gf_get_value_by_label( $form, $entry, $label ) {
-	foreach ( $form['fields'] as $field ) {
-		$lead_key = $field->label;
-		if ( strToLower( $lead_key ) == strToLower( $label ) ) {
-			return $entry[ $field->id ];
-		}
-	}
-	return false;
+add_action('gravityview/edit_entry/after_update', 'gravityview_event_update', 10, 4);
+
+function gravityview_event_update($form, $entry_id, $entry_object) {
+    $post_obj = gform_get_meta($entry_id, "gravityformsadvancedpostcreation_post_id");
+    $post_id = $post_obj[0]["post_id"];
+    $post_data = array(
+        'ID' => $post_id,
+        'post_title' => gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Event Title"),
+        'post_content' => gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Describe What You Do"),
+        'post_category' => gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Experience Type"),
+            //'tags_input' => gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Category Tags")
+    );
+    wp_update_post($post_data);
+    //error_log(print_r(gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Experience Type"), TRUE));
+    //error_log(print_r(gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Category Tags"), TRUE));
+    //error_log("Featured Image: " . print_r(gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Featured Image")), TRUE);
+    require_once(ABSPATH . 'wp-admin/includes/media.php');
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+    $media = media_sideload_image(gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Featured Image"), $post_id);
+    if (!empty($media) && !is_wp_error($media)) {
+        $args = array(
+            'post_type' => 'attachment',
+            'posts_per_page' => -1,
+            'post_status' => 'any',
+            'post_parent' => $post_id
+        );
+        $attachments = get_posts($args);
+        if (isset($attachments) && is_array($attachments)) {
+            foreach ($attachments as $attachment) {
+                $image = wp_get_attachment_image_src($attachment->ID, 'full');
+                // determine if in the $media image we created, the string of the URL exists
+                if (strpos($media, $image[0]) !== false) {
+                    // if so, we found our image. set it as thumbnail
+                    set_post_thumbnail($post_id, $attachment->ID);
+                    break;
+                }
+            }
+        }
+    }
+    // Not sure how else to update all the fields but to mention the by name
+    update_field("about", gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "About You"), $post_id);
+    update_field("location", gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Location"), $post_id);
+    update_field("materials", gf_get_value_by_label($form, GFAPI::get_entry($entry_id), "Experience Materials"), $post_id);
 }
 
+// rather than use potentially changing field ids, look up by label
+function gf_get_value_by_label($form, $entry, $label) {
+    foreach ($form['fields'] as $field) {
+        $lead_key = $field->label;
+        if (strToLower($lead_key) == strToLower($label)) {
+            return $entry[$field->id];
+        }
+    }
+    return false;
+}
 
 add_filter('acf/load_value/type=checkbox', function($value, $post_id, $field) {
-	// Value should be an array, not a string
-	if (is_string($value)) {
-		$value = get_post_meta($post_id, $field['name'], false);
-	}
-	return $value;
+    // Value should be an array, not a string
+    if (is_string($value)) {
+        $value = get_post_meta($post_id, $field['name'], false);
+    }
+    return $value;
 }, 10, 3);
 
 
-add_filter( 'gform_ajax_spinner_url', 'spinner_url', 10, 2 );
-function spinner_url( $image_src, $form ) {
+add_filter('gform_ajax_spinner_url', 'spinner_url', 10, 2);
+
+function spinner_url($image_src, $form) {
     return get_stylesheet_directory_uri() . "/images/makey.png";
 }
