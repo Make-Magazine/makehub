5330135a2376248a8a021936cdb0d31b532c0d14
Alicia Williams <alicia@make.co>
2020-09-10 17:58:48 -0600

    error in js

--- wp-content/themes/make-campus/js/site/gravityForms.js
+++ wp-content/themes/make-campus/js/site/gravityForms.js
@@ -1,18 +1,15 @@
-<script type="text/javascript">
-    jQuery(document).ready(function ($) {
-        jQuery(document).on("change", "li.preview_image div.ginput_container input[type=file]", function () {
-            readURL(this);
-        });
+jQuery(document).ready(function ($) {
+    jQuery(document).on("change", "li.preview_image div.ginput_container input[type=file]", function () {
+        readURL(this);
+    });
 
-        function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    alert(e.target.result);
-                    //jQuery(this).parent().append('<img src="' + e.target.result + '" style="width:150px;"');
-                }
-                reader.readAsDataURL(input.files[0]);
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {                
+                jQuery(this).parent().append('<img src="' + e.target.result + '" style="width:150px;"');
             }
+            reader.readAsDataURL(input.files[0]);
         }
-    })
-</script>
+    }
+})
