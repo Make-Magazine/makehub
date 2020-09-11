e0223b453a1279b012fa9c56fb1a9263482a7f02
Rio Roth-Barrerio <rio@makermedia.com>
2020-09-11 16:24:54 -0700

    gallery plugin and event styling

--- wp-content/themes/make-campus/tribe-events/single-event.php
+++ wp-content/themes/make-campus/tribe-events/single-event.php
@@ -19,49 +19,35 @@ $events_label_singular = tribe_get_event_label_singular();
 $events_label_plural   = tribe_get_event_label_plural();
 
 $event_id = get_the_ID();
-
+$featured_image = tribe_event_featured_image( $event_id, 'full', false, false );
+console
 ?>
 
 <div id="tribe-events-content" class="tribe-events-single">
-	<div class="tribe-events-hero" style="background:url(<?php echo tribe_event_featured_image( $event_id, 'full', false, false ); ?>);">
-		<div class="container">
-			<p class="tribe-events-back">
-				<a href="<?php echo esc_url( tribe_get_events_link() ); ?>" class="btn universal-btn-reversed"> <?php printf( '&laquo; ' . esc_html_x( 'All %s', '%s Events plural label', 'the-events-calendar' ), $events_label_plural ); ?></a>
-			</p>
-			<div class="tribe-events-hero-info tribe-clearfix">
-				<?php the_title( '<h1 class="tribe-events-single-event-title">', '</h1>' ); ?>
-				<?php echo tribe_events_event_schedule_details( $event_id, '<h2>', '</h2>' ); ?>
-				<?php if ( tribe_get_cost() ) : ?>
-					<span class="tribe-events-cost">&nbsp;-&nbsp;<?php echo tribe_get_cost( null, true ) ?></span>
-				<?php endif; ?>
-			</div>
-			
-		</div>
+
+	<div class="tribe-events-header tribe-clearfix">
+		<?php the_title( '<h1 class="tribe-events-single-event-title">', '</h1>' ); ?>
+		<?php echo tribe_events_event_schedule_details( $event_id, '<h2>', '</h2>' ); ?>
+		<?php if ( tribe_get_cost() ) : ?>
+			<span class="tribe-events-cost">&nbsp;-&nbsp;<?php echo tribe_get_cost( null, true ) ?></span>
+		<?php endif; ?>
+		<p class="tribe-events-back">
+			<a href="<?php echo esc_url( tribe_get_events_link() ); ?>" class="btn universal-btn-reversed"> <?php printf( '&laquo; ' . esc_html_x( 'All %s', '%s Events plural label', 'the-events-calendar' ), $events_label_plural ); ?></a>
+		</p>
+	</div>
+
+ 	<div class="tribe-events-image-gallery">
+		<?php echo do_shortcode('[gallery id="'.$event_id.'"]'); ?>
 	</div>
 	
 	<!-- Notices -->
 	<?php tribe_the_notices() ?>
 
-	
-
-	
-	<!-- Event header -->
-	<div id="tribe-events-header" <?php tribe_events_the_header_attributes() ?>>
-		<!-- Navigation -->
-		<nav class="tribe-events-nav-pagination" aria-label="<?php printf( esc_html__( '%s Navigation', 'the-events-calendar' ), $events_label_singular ); ?>">
-			<ul class="tribe-events-sub-nav">
-				<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '<span>&laquo;</span> %title%' ) ?></li>
-				<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% <span>&raquo;</span>' ) ?></li>
-			</ul>
-			<!-- .tribe-events-sub-nav -->
-		</nav>
-	</div>
-	<!-- #tribe-events-header -->
 
 	<?php while ( have_posts() ) :  the_post(); ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?> class="container">
-			<div class="row event-description">
-				<div class="col-xs-12">
+			<div class="row">
+				<div class="col-md-8 col-sm-12 event-info">
 					<!-- Event content -->
 					<?php do_action( 'tribe_events_single_event_before_the_content' ) ?>
 					<div class="tribe-events-single-event-description tribe-events-content">
@@ -73,12 +59,7 @@ $event_id = get_the_ID();
 					<!-- .tribe-events-single-event-description -->
 					<?php do_action( 'tribe_events_single_event_after_the_content' ) ?>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-8 col-sm-12 tribe-events-gallery">
-					<?php echo do_shortcode('[gallery id="'.$event_id.'" exclude="'.get_post_thumbnail_id().'" link="none" size="full"]'); ?>
-				</div>
-				<div class="col-md-4 col-sm-12">
+				<div class="col-md-4 col-sm-12 event-meta">
 					<!-- Event meta -->
 					<?php do_action( 'tribe_events_single_event_before_the_meta' ) ?>
 					<?php tribe_get_template_part( 'modules/meta' ); ?>
