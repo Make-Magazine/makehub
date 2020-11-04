d062736687b968c3cfca4a14ceb646339bcae36f
Rio Roth-Barrerio <rio@makermedia.com>
2020-11-04 11:54:47 -0800

    get the right page top padding for our weird stage url

--- wp-content/universal-assets/v1/js/universal-nav.js
+++ wp-content/universal-assets/v1/js/universal-nav.js
@@ -102,6 +102,7 @@
             case "learn.makehub.staging.wpengine.com":
 			case "experiences.makehub.local":
 			case "experiences.make.co":
+			case "experiencestage.make.co":
 			case "experiences.makehub.wpengine.com":
             case "experiences.makehub.staging.wpengine.com":
 			case "experiences.stagemakehub.wpengine.com":
