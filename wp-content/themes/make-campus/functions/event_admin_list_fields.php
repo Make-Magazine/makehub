f30ddda3adff62b92947f9b394ae043772bbd683
Alicia Williams <alicia@make.co>
2021-01-12 12:03:56 -0700

    adding min attendees (ticket capacity) to the admin event listing page before the attendees column

---
+++ wp-content/themes/make-campus/functions/event_admin_list_fields.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * add min num of participants 
+ */
+//manage_tribe_events_posts_columns
+add_filter('manage_tribe_events_posts_columns', 'make_events_posts_columns');
+
+function make_events_posts_columns($columns) {
+    $new_columns = array();
+    
+    //add Min Attendees column before Attendees in admin event listing
+    foreach ($columns as $key => $value) {
+        if ($key == 'tickets') {
+            $new_columns['min_attendees'] = __('Min Attendees');
+        }
+        $new_columns[$key] = $value;
+    }
+    return $new_columns;
+}
+
+//manage_tribe_events_posts_custom_column
+add_action('manage_tribe_events_posts_custom_column', 'make_events_column', 10, 2);
+
+function make_events_column($column, $post_id) {
+// populate min attendees with _tribe_ticket_capacity field from post
+    if ('min_attendees' === $column) {
+        
+        $min_attendees = get_post_meta($post_id, '_tribe_ticket_capacity', true);
+
+        if (!$min_attendees) {
+            _e('');
+        } else {
+            echo $min_attendees;
+        }
+    }
+}
