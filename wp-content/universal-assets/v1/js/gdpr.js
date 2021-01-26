92a5a0e26fe76738b48d96d050cb485efec3feb7
Rio Roth-Barrerio <rio@makermedia.com>
2021-01-26 14:50:55 -0800

    set gdpr.js to use dev version of the helper iframe

--- wp-content/universal-assets/v1/js/gdpr.js
+++ wp-content/universal-assets/v1/js/gdpr.js
@@ -1,7 +1,7 @@
 // GDPR compliance never looked so good
 window.addEventListener('load', function() {
 
-	var storage = new CrossStorageClient('http://makehub.local/wp-content/universal-assets/v1/page-elements/universal-gdpr-helper.html', {
+	var storage = new CrossStorageClient('http://devmakehub.wpengine.com/wp-content/universal-assets/v1/page-elements/universal-gdpr-helper.html', {
 	  timeout: 90000000,
 	  frameId: 'storageFrame'
 	});
