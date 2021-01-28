3ae4035732ba1e8d7c7b5ec1cdc14210e48e0778
Rio Roth-Barrerio <rio@makermedia.com>
2021-01-28 09:23:55 -0800

    load more missing javascript the universal js relies on

--- wp-content/themes/buddyboss-theme-child/functions.php
+++ wp-content/themes/buddyboss-theme-child/functions.php
@@ -51,6 +51,9 @@ function buddyboss_theme_child_scripts_styles()
   wp_enqueue_style( 'buddyboss-child-css', get_stylesheet_directory_uri().'/assets/css/custom.css', '', '1.0.0' );
 	
   // Javascript
+  wp_enqueue_script('auth0', 'https://cdn.auth0.com/js/auth0/9.3.1/auth0.min.js', array(), false, true );
+  wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), '', true);
+  wp_enqueue_script('fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.6/js/jquery.fancybox.min.js', array('jquery'), '', true);
   wp_enqueue_script('universal', content_url() . '/universal-assets/v1/js/min/universal.min.js', '', '1.0.0' );
   wp_enqueue_script( 'buddyboss-child-js', get_stylesheet_directory_uri().'/assets/js/custom.js', '', '1.0.0' );
 }
