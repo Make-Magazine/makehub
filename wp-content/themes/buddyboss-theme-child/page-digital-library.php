c14702f016fda1811316f95dbf3e8da46182580f
Alicia Williams <alicia@make.co>
2021-02-16 10:52:55 -0700

    adding digital library and video gallery pages

---
+++ wp-content/themes/buddyboss-theme-child/page-digital-library.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template Name: Digital-Library pages
+ */
+
+get_header(); ?>
+
+<div id="page-content" class="mmBack bluetoad">
+
+        <?php // theloop
+        if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();  ?>
+          <?php 
+          /*  $results =  $vendor->addSubscriber($email, 494256);
+            $results =  $vendor->addSubscriber($email, 478666);
+            $results =  $vendor->addSubscriber($email, 466306);
+            $results =  $vendor->addSubscriber($email, 449478);
+            $results =  $vendor->addSubscriber($email, 478666);
+            $results =  $vendor->addSubscriber($email, 434632);
+            $results =  $vendor->addSubscriber($email, 369632);
+            $results =  $vendor->addSubscriber($email, 355660);
+            $results =  $vendor->addSubscriber($email, 333989);
+            $results =  $vendor->addSubscriber($email, 317354);
+            $results =  $vendor->addSubscriber($email, 285363);
+            $results =  $vendor->addSubscriber($email, 301846);
+            $results =  $vendor->addSubscriber($email, 301454);
+            $results =  $vendor->addSubscriber($email, 301840);  
+          
+          $vendor = new PdfVendor();
+          $email = 'webmaster@make.co';
+          $results = $vendor->showListOfPdfs($email); */
+				
+          ?>
+			<iframe id="bluetoad-iframe" src="/wp-content/themes/make-co/blue-toad-login.php"></iframe>
+          <?php the_content(); ?>
+         <?php } // END WHILE ?>
+        <?php } else { ?>
+          <?php get_404_template(); ?>
+        <?php } // END IF ?>
+
+ </div><!-- end .page-content -->
+
+<script type="text/javascript">
+jQuery(document).ready(function(){
+   var is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+   var is_iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+   if (is_safari || is_iOS) {
+      window.location = "/wp-content/themes/make-co/blue-toad-login.php";
+   }
+   jQuery( ".page-template-page-digital-library footer" ).mouseleave( function() {
+		jQuery( [document.documentElement, document.body] ).animate({
+			scrollTop: jQuery( "#page-content.bluetoad iframe" ).offset().top
+		}, 100);
+	});
+});
+</script>
+
+<?php
+genesis_structural_wrap( 'site-inner', 'close' );
+genesis_markup(
+	array(
+		'close'   => '</div>',
+		'context' => 'site-inner',
+	)
+);
+wp_footer();
+?>
---
+++ wp-content/themes/buddyboss-theme-child/page-videogallery.php
@@ -0,0 +1,52 @@
+<?php 
+ /**
+ * Template Name: Video Gallery
+ *
+ * @version 1.0
+ */
+   
+if (!is_user_logged_in()) { // Make sure membership is active
+	auth_redirect();
+}
+get_header(); 
+$querystr = "
+	SELECT $wpdb->vimeography_gallery.* 
+	FROM $wpdb->vimeography_gallery
+";
+$galleryInfo = $wpdb->get_results($querystr, OBJECT);
+// get the first value, since the first gallery won't always have id = 1
+$first = true;
+$galleryID = 1;
+foreach ( $galleryInfo as $gallery ){
+	if ( $first ){
+		$first = false;
+		$galleryID = $gallery->id;
+	}
+}
+if(isset($_GET['gallery'])){
+	$galleryID = $_GET['gallery'];
+}
+$pageUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+?>  
+
+<div class="vimeography-header">
+   <h1>Welcome to the members-only video playlists.</h1>
+   <p>Enjoy hundreds of hours of presentations and talks from recent Maker Faires. We'll be adding more, so check back often.</p>
+   <p>Now viewing:</p>
+   <form id="vimeography-galleries" action="<?php echo($pageUrl); ?>" method="get">
+      <div class="select">
+         <select name="gallery" onchange="this.form.submit();">
+            <?php foreach ($galleryInfo as $gallery) { ?>
+            <option value="<?php echo($gallery->id); ?>" <?php if($galleryID == $gallery->id){echo("selected");} ?>><?php echo($gallery->title); ?></option>
+            <?php } ?>
+         </select>
+      </div>
+   </form>
+</div>
+
+<div class="container page-content video-gallery"> 
+   <?php echo(do_shortcode('[vimeography id="' . $galleryID . '"]')); ?>
+</div><!-- end .page-content -->
+
+<?php get_footer(); ?>
