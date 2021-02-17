28ff9f64c1aae530deec3a9922ca783c4b392d71
Alicia Williams <alicia@make.co>
2021-02-17 16:41:59 -0700

    redirect all staging and dev emails to webmaster

---
+++ wp-content/themes/buddyboss-theme-child/functions/stagingNotification.php
@@ -0,0 +1,16 @@
+<?php
+
+// override for the wp email function on stage/dev sites
+add_filter('wp_mail', 'change_email_for_wp', 10, 2);
+
+function change_email_for_wp($email) {
+   $homeurl = get_home_url();
+   //check for our stage and dev sites
+   if(strpos($homeurl,'wpengine.com')!==false){     
+       $email['to'] = 'webmaster@make.co';
+       $email['subject'] = 'Stage/Dev Redirect Email - '.$email['subject'];              
+   }
+   
+   return ($email);
+   
+}
