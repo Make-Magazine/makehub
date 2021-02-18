d2d6464a107ba3b7b941dd89a364301e7687d625
Alicia Williams <alicia@make.co>
2021-02-18 15:03:31 -0700

    first attempt at pulling in mf data

---
+++ wp-content/themes/buddyboss-theme-child/db-connect/mf-config.php
@@ -0,0 +1,9 @@
+<?php
+# Database Configuration
+define( 'DB_NAME', 'wp_makerfaire' );
+define( 'DB_USER', 'makerfaire' );
+define( 'DB_PASSWORD', '9i0gNfNKuQWLq6K5' );
+define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST_SLAVE', '127.0.0.1' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', 'utf8_unicode_ci' );
