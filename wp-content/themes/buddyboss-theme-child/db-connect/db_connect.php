bbd73aaa0d033fee7346b27818db8c4c3af85665
Alicia Williams <alicia@make.co>
2021-02-18 15:26:02 -0700

    sigh helps if i connect to the mf db and not the makehub db

--- wp-content/themes/buddyboss-theme-child/db-connect/db_connect.php
+++ wp-content/themes/buddyboss-theme-child/db-connect/db_connect.php
@@ -3,7 +3,7 @@
  * this file sets up a new mysqli connection based on the database defined in a previous step
  */
 
-$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD, DB_NAME);
+$mysqli = new mysqli($host,$user,$password, $database);
 if ($mysqli->connect_errno) {
   echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
