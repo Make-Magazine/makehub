12ec6f69003399af5491c2e31c6b7c4d47902183
Alicia Williams <alicia@make.co>
2021-02-18 15:52:03 -0700

    pull in and display maker info

--- wp-content/themes/buddyboss-theme-child/functions/makerfaire-profile-section.php
+++ wp-content/themes/buddyboss-theme-child/functions/makerfaire-profile-section.php
@@ -47,107 +47,22 @@ function makerfaire_info_content() {
     //access the makerfaire database.    
     include(get_stylesheet_directory() . '/db-connect/mf-config.php');
     include(get_stylesheet_directory() . '/db-connect/db_connect.php');
-    $sql = "SELECT * FROM `wp_mf_maker`";
-    //loop thru entry data
+    
+    //pull maker information from database.    
+    $sql = 'SELECT  wp_mf_maker_to_entity.entity_id, wp_mf_maker_to_entity.maker_type, '
+            . '     wp_mf_maker_to_entity.maker_role, wp_mf_entity.presentation_title, '
+            . '     wp_mf_entity.status, wp_mf_entity.faire '
+         . 'FROM `wp_mf_maker` '
+         . 'left outer join wp_mf_maker_to_entity on wp_mf_maker_to_entity.maker_id = wp_mf_maker.maker_id '
+         . 'left outer join wp_mf_entity on wp_mf_maker_to_entity.entity_id = wp_mf_entity.lead_id '
+         . 'where Email like "'.$user_email.'" and wp_mf_entity.status="Accepted"';
     $entries = $mysqli->query($sql) or trigger_error($mysqli->error . "[$sql]");
     $entryData = array();
     foreach ($entries as $entry) {
-        var_dump($entry);
-        die();
+        echo $entry['faire'].' '.$entry['entity_id'].' '.             
+                '<a target="_blank" href="https://makerfaire.com/maker/entry/'.$entry['entity_id'].'/">'.$entry['presentation_title'].'</a> '.
+                '<br/>';
     }    
-    die();
-        $entry = array();
-        foreach ($results as $row) {
-            if(is_decimal($row->meta_key)){
-                $entry[floor($row->meta_key)][$row->meta_key] = $row->meta_value;
-            }else{
-                $entry[$row->meta_key] = $row->meta_value;            
-            }
-        }        
-        
-        //id, label, type, other
-        $fieldArray = array(                
-                array('15', 'What days is the makerspace open?', 'checkbox',''),
-                array('96', 'Is your space located in a:', 'checkbox','97'),
-                array('98', 'Does it cost money to use this makerspace?',	'yesno','99','100'),
-                array('101', 'What ages are allowed to use the makerspace?', 'checkbox',''),
-                array('102', 'Does the makerspace offer safety and basic use training?', 'radio',''),
-                array('103', 'What kind of classes does the makerspace offer?', 'checkbox', '104'),
-                array('105', 'Does the makerspace provide safety equipment (goggles, welding gloves & jackets, ear protection, etc.)?', 'radio',''),
-                array('107', 'Does the makerspace have a kitchen for use by members?', 'radio', ''),
-                array('123', 'Does the makerspace offer:', 'checkbox', '124'),
-                array('125', 'Tools/materials provided for:', 'checkbox', '126'),
-                array('68', 'Woodworking Equipment:', 'yesno','109','110'),
-                array('67', 'Metalworking Equipment:', 'yesno','111','112'),
-                array('113', 'Welding Equipment:', 'yesno','114','115'),
-                array('65', 'Electronic Equipment:', 'yesno','116','117'),
-                array('74', 'Textile Equipment:', 'yesno','118','119'),
-                array('120', 'Welding Equipment:', 'yesno','121','122'),
-            );
-        
-        echo '<div class="ms_info">';
-        foreach($fieldArray as $field){
-            $fieldID     = $field[0];            
-            if(isset($entry[$fieldID])){
-                echo '<div class="fieldInfo">';
-                $fieldLabel  = $field[1];
-                $fieldType   = $field[2];
-                $fieldOther1 = (isset($field[3])?$field[3]:'');
-                $fieldOther2 = (isset($field[4])?$field[4]:'');
-                //echo $fieldID.' '. $fieldLabel.' '.$fieldType.'<br/>';
-                //radio field
-                if($fieldType == 'radio'){
-                    echo '<span class="fieldLabel">'.$fieldLabel.'</span>';
-                    echo '<div class="fieldData">'.$entry[$fieldID].'</div>';
-                }
-                
-                //checkbox
-                if($fieldType == 'checkbox'){
-                    echo '<span class="fieldLabel">'.$fieldLabel.'</span>';
-                    echo '<ul>';
-                    foreach($entry[$fieldID] as $value){
-                        echo '<li>'.$value.'</li>';
-                    }
-                    
-                    if(isset($entry[$fieldOther1])) {
-                        //if the other field is an array loop through it
-                        if(is_array($entry[$fieldOther1])){                       
-                            foreach($entry[$fieldOther1] as $other_value){
-                                echo '<li>'.$other_value.'</li>';
-                            }
-                        }else{
-                            echo '<li>'.$entry[$fieldOther1].'</li>';
-                        }
-                    }
-                    echo '</ul>';                    
-                }
-
-                // yes/no fields
-                if($fieldType == 'yesno'){
-                    if($entry[$fieldID]=='Yes'){
-                       echo '<span class="fieldLabel">'.$fieldLabel.'</span>';
-                       echo '<ul>';
-                       foreach($entry[$fieldOther1] as $value){
-                           echo '<li>'.$value.'</li>';
-                       }
-
-                       if(isset($entry[$fieldOther2])) {
-                           //if the other field is an array loop through it
-                           if(is_array($entry[$fieldOther2])){                       
-                               if(isset($entry[$fieldOther2])){                
-                                    echo '<li>'.$entry[$fieldOther2].'</li>';
-                                }
-                           }else{
-                               echo '<li>'.$entry[$fieldOther2].'</li>';
-                           }
-                       }
-                       echo '</ul>';
-                    }                    
-                }     
-                echo '</div>';
-            }
-        }                        
-        echo '</div>';
-        
+                                                  
 }
 
