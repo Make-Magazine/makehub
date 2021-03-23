50a7eda868f78da6446ae566e7040f6ef0465af1
Rio Roth-Barrerio <rio@makermedia.com>
2021-03-23 15:12:01 -0700

    ultimate membership pro Membership page with button to manage stripe

---
+++ wp-content/themes/make-experiences/functions/membership-profile-section.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * View and/or edit your membership subscription
+ */
+
+function profile_tab_membership_infoname() {
+    global $bp;
+    $user_id = bp_displayed_user_id();
+    $type = bp_get_member_type(bp_displayed_user_id());
+    if ($user_id != 0 && wp_get_current_user()->ID == $user_id ) {
+        bp_core_new_nav_item(array(
+            'name' => 'Membership',
+            'slug' => 'membership',
+            'screen_function' => 'membership_info_screen',
+            'position' => 40,
+            'parent_url' => bp_loggedin_user_domain() . '/membership/',
+            'parent_slug' => $bp->profile->slug,
+            'default_subnav_slug' => 'membership'
+        ));
+    }
+}
+
+add_action('bp_setup_nav', 'profile_tab_membership_infoname');
+
+function membership_info_screen() {
+    // Add title and content here - last is to call the members plugin.php template.
+    //add_action('bp_template_title', 'membership_info_title');
+    add_action('bp_template_content', 'membership_info_content');
+    bp_core_load_template('buddypress/members/single/plugins');
+}
+
+function membership_info_title() {
+    //echo 'Maker Faire Information';
+}
+
+function membership_info_content() {
+    global $wpdb;
+
+    $user_id = bp_displayed_user_id();
+    //get the users email
+    $user_info = get_userdata($user_id);
+    $user_email = $user_info->user_email;
+
+	require_once(get_stylesheet_directory() . '/vendor/stripe/stripe-php/init.php');
+	\Stripe\Stripe::setApiKey('sk_test_2p3NPkMLrR81uL09daZDxnMg');
+
+	$customer = \Stripe\Customer::all(["email" => $user_email]);
+	$customerID = $customer->data[0]['id'];
+
+	$session = \Stripe\BillingPortal\Session::create([
+	  'customer' => $customerID,
+	  'return_url' => 'https://makehub.local/account',
+	]);
+	
+	echo '<div class="membership-tab-wrapper">';
+		echo '<h1>Current Make: Membership</h1>';
+		echo '<a href="' . $session->url . '" class="btn universal-btn" id="manage-membership-btn" target="_blank">Manage Subscription</a>';
+		if (!class_exists('ihcAccountPage')){
+			require_once IHC_PATH . 'classes/ihcAccountPage.class.php';
+		}
+		$obj = new ihcAccountPage($attr);
+		echo $obj->print_page("orders");
+	echo '</div>';
+}
