ccf1558aade393372dd8acc2dd166395ea4db52a
Alicia Williams <alicia@make.co>
2021-04-05 13:14:01 -0600

    more changes to set up the EE event

--- wp-content/themes/make-experiences/functions/events-create.php
+++ wp-content/themes/make-experiences/functions/events-create.php
@@ -10,118 +10,151 @@ function disable_post_creation($is_disabled, $form, $entry) {
 // Create event with ticket
 add_action('gform_after_submission_7', 'create_event', 10, 2);
 
-function create_event($entry, $form) {
-    //let's get some basic info for the event
-    //pull field by variable name
-    //Schedule is stored in a nested form
-    var_dump($entry);
+function create_event($entry, $form) {    
+    //get current user info 
+    global $current_user;
+    $current_user = wp_get_current_user();
+    $userID     = $current_user->ID;
+    $userEmail  = (string) $current_user->user_email;
     
-    //Start Date
-    $date       = date_create($entry['4'] . ' ' . $entry['5']); 
-    $start_date = date_format($date,"Y-m-d"). 'T'.date_format($date,"H:i:s"); //date format needs to be 2021-03-25T18:00:00    
-    
-    //End Date
-    $date     = date_create($entry['4'] . ' ' . $entry['7']);
-    $end_date = date_format($date,"Y-m-d"). 'T'.date_format($date,"H:i:s"); //date format needs to be 2021-03-25T18:00:00
-            
-    //Create the event
-    $rest_endpoint = '/ee/v4.8.36/events';
-    $body_params = array('EVT_name' => $entry[1],
-        'EVT_desc' => $entry[2],
-        'EVT_short_desc' => $entry[119],
-        'EVT_wp_user' => 3128,
-        'status' => "pending"
-    );
-    $data = int_rest_req($rest_endpoint, $body_params);
-    //was the event created?
-    if (!empty($data['EVT_ID'])) {        
-        $event_id = $data['EVT_ID'];
+    //first create the event
+    $currDateTime = date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000));
+    $event = EE_Event::new_instance(
+                    array('EVT_name' => $entry[1],
+                        'EVT_desc' => $entry[2],
+                        'EVT_short_desc' => $entry[119],
+                        'EVT_wp_user' => $userID,
+                        'status' => "pending",
+                        'EVT_visible_on' => $currDateTime
+    ));
+    $event->save();
+    $eventID = $event->ID();
 
-        //next set the date/time of the event
-        $rest_endpoint = '/ee/v4.8.36/datetimes';
-        $body_params = array('EVT_ID' => $event_id,
-                            'DTT_name' => $entry['1'],
-                            'DTT_EVT_start' => $start_date, //"Start time/date of Event - this is in the timezone of the site."
-                            'DTT_EVT_end' => $end_date, //"End time/date of Event - this in the timezone of the site."
-                            'DTT_reg_limit' => $entry['106'], //"Registration Limit for this time"
-        );
-        $data = int_rest_req($rest_endpoint, $body_params);
+    /* Event Date/Time and Tickets
+     *      Ticket and schedule information is set in a nested form
+     *      Need to get nested form ID and then loop through the nested form information 
+     */
 
+    //pull field by variable name 
+    $parameter_array = find_field_by_parameter($form);
+    $timeZone = getFieldByParam('timezone', $parameter_array, $entry);
 
-        //was the datetime created?
-        if (!empty($data['DTT_ID'])) {
-            $dateTimeID = $data['DTT_ID'];
+    //pull nested form to get submitted schedule/ticket
+    if (isset($parameter_array['nested-form'])) {
 
-            //now let's create the ticket            
-            $rest_endpoint = '/ee/v4.8.36/tickets';
-            $body_params = array('TKT_name' => "Ticket - " . $entry['1'],
-                'TKT_description' => (isset($entry['42']) ? $entry['42'] : ''),
-                'TKT_price' => (isset($entry['37']) ? $entry['37'] : 0),
-                'TKT_min' => (isset($entry['43']) ? $entry['43'] : ''),
-                'TKT_max' => (isset($entry['106']) ? $entry['106'] : ''),
-                'TKT_qty' => (isset($entry['106']) ? $entry['106'] : ''), //"Quantity of this ticket that is available"
-                'TKT_required' => true);
-            $data = int_rest_req($rest_endpoint, $body_params);
+        $nstFormID = (isset($parameter_array['nested-form']['gpnfForm']) ? $parameter_array['nested-form']['gpnfForm'] : 10);
+        $nstForm = GFAPI::get_form($nstFormID);
 
-            //was the ticket created?
-            if (!empty($data['TKT_ID'])) {                
-                $ticketID = $data['TKT_ID'];
+        //get the list of entry id's for the nested form
+        $nstEntryIDs = $entry[$parameter_array['nested-form']['id']];
+        $nstEntryArr = explode(",", $nstEntryIDs);
+        foreach ($nstEntryArr as $nstEntryID) {
+            $nst_entry = GFAPI::get_entry($nstEntryID);
+            $nest_parameter_arr = find_field_by_parameter($nstForm); //find all fields with paramater names set in nested form
+            //Ticket Information        
+            $value = getFieldByParam('ticket-name', $nest_parameter_arr, $nst_entry);
+            $ticketName = (!empty($value) ? $value : 'Ticket - ' . $entry[1]); //if ticket name not given, default ticket name to 'Ticket - Event Name'
 
-                //then relate the datetime with the ticket
-                $rest_endpoint = '/ee/v4.8.36/datetime_tickets';
-                $body_params = array('DTT_ID' => $dateTimeID, 'TKT_ID' => $ticketID);
-                $data = int_rest_req($rest_endpoint, $body_params);
+            $ticketPrice = getFieldByParam('ticket-price', $nest_parameter_arr, $nst_entry);
+            $ticketDesc  = getFieldByParam('ticket-desc', $nest_parameter_arr, $nst_entry);
+            $ticketMin   = getFieldByParam('ticket-min', $nest_parameter_arr, $nst_entry);
+            $ticketMax   = getFieldByParam('ticket-max', $nest_parameter_arr, $nst_entry);
+            $schedDesc   = getFieldByParam('sched-desc', $nest_parameter_arr, $nst_entry);
+            //create the ticket instance
+            $tkt = EE_Ticket::new_instance(array('TKT_name' => $ticketName,
+                        'TKT_description' => $ticketDesc,
+                        'TKT_price' => $ticketPrice,
+                        'TKT_min' => $ticketMin,
+                        'TKT_max' => $ticketMax,
+                        'TKT_qty' => $ticketMax, //"Quantity of this ticket that is available"
+                        'TKT_required' => true));
+            $tkt->save();
 
-                //now create a price object       
-                $rest_endpoint = '/ee/v4.8.36/prices';
-                $body_params = array('PRT_ID' => 1, 'PRC_amount' => (isset($entry['37']) ? $entry['37'] : 0));
-                $data = int_rest_req($rest_endpoint, $body_params);
+            //create Price object
+            $price = EE_Price::new_instance(array('PRT_ID' => 1, 'PRC_amount' => $ticketPrice));
+            $price->save();
+            $tkt->_add_relation_to($price, 'Price'); //link the price and ticket instances
+            //Schedule Info
+            $prefSchedSer = getFieldByParam('preferred-schedule', $nest_parameter_arr, $nst_entry);
+            $altSchedSer = getFieldByParam('alternative-schedule', $nest_parameter_arr, $nst_entry);
 
-                //was price object created?
-                if (!empty($data['PRC_ID'])) {
-                    $priceID = $data['PRC_ID'];
-                    
-                    //associate that price object to the ticket                    
-                    $rest_endpoint = '/ee/v4.8.36/ticket_prices';
-                    $body_params = array('TKT_ID' => $ticketID, 'PRC_ID' => $priceID);
-                    $data = int_rest_req($rest_endpoint, $body_params);
-                }
+            //Note we need to do something more secure here to avoid code injection
+            $prefSched = unserialize($prefSchedSer);
+
+            //create tickets
+            foreach ($prefSched as $sched) {
+                //TBD - do we need to convert timezone as events are saved in the timezone of the site - Pacific
+                //Start Date
+                $date = date_create($sched['Date'] . ' ' . $sched['Start Time']);
+                $start_date = new DateTime(date_format($date, "Y-m-d") . 'T' . date_format($date, "H:i:s"), new DateTimeZone($timeZone));
+
+                //End Date
+                $date = date_create($sched['Date'] . ' ' . $sched['End Time']);
+                $end_date = new DateTime(date_format($date, "Y-m-d") . 'T' . date_format($date, "H:i:s"), new DateTimeZone($timeZone));
+
+                //create the date/time instance
+                $d = EE_Datetime::new_instance(
+                                array('EVT_ID' => $eventID,
+                                    'DTT_name' => $schedDesc,
+                                    'DTT_EVT_start' => $start_date, 'DTT_EVT_end' => $end_date, 'DTT_reg_limit' => $ticketMax));
+
+                $d->save();
+                $tkt->_add_relation_to($d, 'Datetime'); //link the datetime and the ticket instances
             }
         }
-
-        //check if user exists
-        //if they do, assign that user to this event
-        //if they do not, add user
-        //then assign that uer to this event
-    }else{
-        //event was not created - TBD need a fallback here
-        error_log('Error in creating the event for entry '. $entry('ID'));
-        return;
     }
     
+    $userBio   = getFieldByParam('user-bio', $parameter_array, $entry);
+    $userFname = getFieldByParam('user-fname', $parameter_array, $entry);
+    $userLname = getFieldByParam('user-lname', $parameter_array, $entry);
+    
+    //check if facilitator exists
+    $person = EEM_Person::instance()->get_one([['PER_email' => $userEmail]]);
+
+    if ($person) {
+        $personID = $person->ID();
+        //update bio fname and lname if changed
+    } else { //if they do not exist, add user
+        $person = EE_Person::new_instance(array(
+                    "PER_full_name" => $userFname.' '.$userLname,
+                    "PER_bio" => $userBio,
+                    "PER_fname" => $userFname,
+                    "PER_lname" => $userLname,                    
+                    "PER_email" => $userEmail
+        ));
+        $person->save();
+        $personID = $person->ID();
+    }
+    
+    //assign that user to this event
+    $per_post = EE_Person_Post::new_instance(array('PER_ID' => $personID, 'OBJ_ID' => $eventID, 'PT_ID' => '67'));
+    $per_post->save();
+
     /*
      * Now that the event is created, let's transfer data from the entry to the event
      */
-    // update taxonomies, featured image, etc
-    event_post_meta($entry, $form, $event_id);
-    // Set the ACF data
-    update_event_acf($entry, $form, $event_id);
-    // Set event custom fields for filtering
-    update_event_additional_fields($entry, $form, $event_id);
+    /*
+      // update taxonomies, featured image, etc
+      event_post_meta($entry, $form, $event_id);
+      // Set the ACF data
+      update_event_acf($entry, $form, $event_id);
+      // Set event custom fields for filtering
+      update_event_additional_fields($entry, $form, $event_id); */
 }
 
 /*  This function performs the internal rest requests to Event Espresso */
-function int_rest_req($rest_endpoint, $body_params, $type='POST') {
+
+function int_rest_req($rest_endpoint, $body_params, $type = 'POST') {
     $request = new WP_REST_Request($type, $rest_endpoint);
     $request->set_body_params($body_params);
 
     $response = rest_do_request($request);
     $server = rest_get_server();
-    $data = $server->response_to_data($response, false);                
-    
-    if ( $response->is_error() ) {
-        echo 'Error in call to WPI Rest API endpoint ('.$rest_endpoint.')<br/>';
+    $data = $server->response_to_data($response, false);
+
+    if ($response->is_error()) {
+        echo 'Error in call to WPI Rest API endpoint (' . $rest_endpoint . ')<br/>';
         var_dump($data);
-    }    
+    }
     return $data;
 }
--- wp-content/themes/make-experiences/functions/gravityForms.php
+++ wp-content/themes/make-experiences/functions/gravityForms.php
@@ -1,36 +1,96 @@
 <?php
+
 //This filter declaration targets form 10 - field 9 - the last digit is the column in the list field
-add_filter( 'gform_column_input_content_10_9_1', 'set_date_field_type', 10, 6 );
-add_filter( 'gform_column_input_content_10_9_2', 'set_time_field_type', 10, 6 );
-add_filter( 'gform_column_input_content_10_9_3', 'set_time_field_type', 10, 6 );
+add_filter('gform_column_input_content_10_9_1', 'set_date_field_type', 10, 6);
+add_filter('gform_column_input_content_10_9_2', 'set_time_field_type', 10, 6);
+add_filter('gform_column_input_content_10_9_3', 'set_time_field_type', 10, 6);
 
 //This filter declaration targets form 10 - field 11 - the last digit is the column in the list field
-add_filter( 'gform_column_input_content_10_11_1', 'set_date_field_type', 10, 6 );
-add_filter( 'gform_column_input_content_10_11_2', 'set_time_field_type', 10, 6 );
-add_filter( 'gform_column_input_content_10_11_3', 'set_time_field_type', 10, 6 );
+add_filter('gform_column_input_content_10_11_1', 'set_date_field_type', 10, 6);
+add_filter('gform_column_input_content_10_11_2', 'set_time_field_type', 10, 6);
+add_filter('gform_column_input_content_10_11_3', 'set_time_field_type', 10, 6);
 
 //reformat field as date type
-function set_date_field_type( $input, $input_info, $field, $text, $value, $form_id ) {
-    $form = GFAPI::get_form($form_id);
-    var_dump($form);
+function set_date_field_type($input, $input_info, $field, $text, $value, $form_id) {
     //build field name, must match List field syntax to be processed correctly
     $input_field_name = 'input_' . $field->id . '[]';
-    $input_field_id = 'input_'.$form_id.'_'.$field->id;
+    $input_field_id = 'input_' . $form_id . '_' . $field->id;
     $tabindex = GFCommon::get_tabindex();
-    
-    $new_input = '<input name="' . $input_field_name . '" '.$tabindex.' id="'.$input_field_id.'" type="text" value="'.$value.'" class="datepicker medium mdy datepicker_no_icon hasDatepicker" tabindex="100002" aria-describedby="input_10_12_date_format">'.
-                 ' <span id="'.$input_field_id.'_date_format" class="screen-reader-text">Date Format: MM slash DD slash YYYY</span>';    
-    return $new_input;    
+
+    $new_input = '<input name="' . $input_field_name . '" ' . $tabindex . ' id="' . $input_field_id . '" type="text" value="' . $value . '" class="datepicker medium mdy datepicker_no_icon hasDatepicker" tabindex="100002" aria-describedby="input_10_12_date_format">' .
+            ' <span id="' . $input_field_id . '_date_format" class="screen-reader-text">Date Format: MM slash DD slash YYYY</span>';
+    return $new_input;
 }
 
 //reformat field as time type
-function set_time_field_type( $input, $input_info, $field, $text, $value, $form_id ) {    
+function set_time_field_type($input, $input_info, $field, $text, $value, $form_id) {
     $tabindex = GFCommon::get_tabindex();
     $input_field_name = 'input_' . $field->id . '[]';
-    $input_field_id = 'input_'.$form_id.'_'.$field->id;
-    
-    $new_input = '<input type="time" name="'.$input_field_name.'" value="'.$value.'" '.$tabindex.' step="900" >';    //15 minute increments
-                        
-    return $new_input;    
+    $input_field_id = 'input_' . $form_id . '_' . $field->id;
+
+    $new_input = '<input type="time" name="' . $input_field_name . '" value="' . $value . '" ' . $tabindex . ' step="900" >';    //15 minute increments
+
+    return $new_input;
 }
 
+function find_field_by_parameter($form) {
+    $parameter_array = array(); //array of field id's and their associated parameter name
+    if (isset($form['fields'])) {
+        foreach ($form['fields'] as $field) {
+            //paramater names are stored in a different place
+            if ($field['type'] == 'name' || $field['type'] == 'address' || $field['type'] == 'checkbox') {
+                foreach ($field['inputs'] as $choice) {
+                    if ($choice['name'] != '')
+                        $parameter_array[$choice['name']] = $field;
+                }
+            }
+            if ($field['allowsPrepopulate'] && $field['inputName'] != '') {
+                $parameter_array[$field['inputName']] = $field;
+            }
+        }
+    }
+    return $parameter_array;
+}
+
+function getFieldByParam($paramName = '', $parameterArray = array(), $entry = array()) {
+    $field = $parameterArray[$paramName];
+    if (isset($field)) {
+        if ($field->type == 'name' || $field->type == 'address' || $field->type == 'checkbox') {
+            foreach ($field->inputs as $choice) {
+                if ($choice['name'] == $paramName) {
+                    $fieldID = $choice['id'];
+                }
+            }
+        } else {
+            $fieldID = $field->id;
+        }
+        return (isset($entry[$fieldID]) ? $entry[$fieldID] : '');
+    }
+    return '';
+}
+
+/* this field will prepoulate gravity form fields based on the set parameter name */
+add_filter('gform_field_value', 'set_field_values', 10, 3);
+
+function set_field_values($value, $field, $name) {
+    $values = array();
+    if(!empty($name)) {
+        //check if facilitator exists
+        global $current_user;
+        $current_user = wp_get_current_user();
+        $userEmail = (string) $current_user->user_email;        
+        $person = EEM_Person::instance()->get_one([['PER_email' => $userEmail]]);
+        if ($person) {
+            if($name=='user-fname'){
+                //var_dump($person);
+            }
+            $values = array(
+                'user-fname' => $person->fname(),
+                'user-lname' => $person->lname(),
+                    //'user-bio' => $person->bio(),
+            );
+        }
+    }
+
+    return isset($values[$name]) ? $values[$name] : $value;
+}
