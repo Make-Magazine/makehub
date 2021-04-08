0df4d51856a8eb3cc50c9c67d7240ff0921dd5b4
Rio Roth-Barrerio <rio@makermedia.com>
2021-04-08 14:48:32 -0700

    finish up the events list view

--- wp-content/themes/make-experiences/page-espresso_events-list.php
+++ wp-content/themes/make-experiences/page-espresso_events-list.php
@@ -29,19 +29,50 @@ get_header();
 				 	$dateFormat = date('D <\b/>j<\/b/>', strtotime($date->start_date()));
 					$startime = date('F j, Y @ g:i a', strtotime($date->start_time()));
 					$endime = date('g:i a', strtotime($date->end_time()));
-					$return = '<a href="' . get_permalink() . '">
-							     <article id="post-' . $post->ID . '" '. esc_attr( implode( ' ', get_post_class() ) )  .'>
-								   <div class="event-truncated-date">' . $dateFormat . '</div>
-								   <div class="event-image">
-								   	 <img src="' . get_the_post_thumbnail_url( $post->ID, 'thumbnail' ) . '" />
-								   </div>
-								   <div class="event-info">
-								   	 <div class="event-date">'. $startime . ' - ' . $endime . '</div>
-									 <div class="event-title"></div>
-								   </div>';
-									
-					$return .= ' </article>
-							   </a>';
+					// grab array of EE_Ticket objects for event
+					$tickets = EEH_Event_View::event_tickets_available( $post->ID );
+					if ( is_array( $tickets ) && count($tickets) > 1 ) {
+						foreach($tickets as  $ticket => $element) {
+							$tickets[$ticket] = preg_replace('/<span[^>]*>([\s\S]*?)<\/span[^>]*>/', '', $tickets[$ticket]->pretty_price());
+						}
+					}
+					sort($tickets);
+				  	$ticket_price = 'Tickets Not Available';
+					if ( is_array( $tickets ) && count($tickets) > 1 ) {
+						foreach($tickets as $ticket => $element) {
+							reset($tickets);
+   							if ($ticket === key($tickets))
+								$ticket_price = $tickets[$ticket];
+							
+							end($tickets);
+							if ($ticket === key($tickets) && $tickets[$ticket] != $ticket_price)
+								$ticket_price .= "-" . $tickets[$ticket];
+						}
+					} else if (count($tickets) > 0) {
+						$ticket_price = $tickets[0];
+					}
+					$return = '<article id="post-' . $post->ID . '" '. esc_attr( implode( ' ', get_post_class() ) )  .'>
+							     <div class="event-truncated-date">' . $dateFormat . '</div>
+							     <div class="event-image">
+								   <a href="' . get_permalink() . '">
+								     <img src="' . get_the_post_thumbnail_url( $post->ID, 'thumbnail' ) . '" />
+								   </a>
+							     </div>
+							     <div class="event-info">
+								   <div class="event-date">'. $startime . ' - ' . $endime . '</div>
+								   <h3 class="event-title">
+								     <a href="' . get_permalink() . '">' . get_the_title() . '</a>
+								   </h3>
+								   <div class="event-description">' . get_field('short_description') . '</div
+								   <div class="event-prices">';
+										if($ticket_price != 'Tickets Not Available') {
+											$return .= '<a href="' . get_permalink() . '" class="btn universal-btn">Get Tickets</a>';
+										}
+										$return .= $ticket_price . 
+								  '</div>
+								 </div>
+							   </article>';
+
 				    echo $return;
 
 				endwhile;
