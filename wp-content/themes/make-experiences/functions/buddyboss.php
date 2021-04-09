8a4b150879e1e989e93827af9673c1b6a381641a
Alicia Williams <alicia@make.co>
2021-04-09 14:52:15 -0600

    remove blogs from profile nav

--- wp-content/themes/make-experiences/functions/buddyboss.php
+++ wp-content/themes/make-experiences/functions/buddyboss.php
@@ -8,3 +8,11 @@ function set_displayed_user($user_id) {
     $bp->displayed_user->userdata = bp_core_get_core_userdata($bp->displayed_user->id);
     $bp->displayed_user->fullname = bp_core_get_user_displayname($bp->displayed_user->id);
 }
+
+//remmove the blog from profile tabs
+function remove_profile_nav() {
+    global $bp;
+    bp_core_remove_nav_item('blog');
+}
+
+add_action('bp_init', 'remove_profile_nav');
