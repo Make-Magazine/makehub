ff7b0a08969ae27733dca5a8248909054fa588e3
Rio Roth-Barrerio <rio@makermedia.com>
2021-04-10 21:21:01 -0700

    facilitator host page featured image and list styling

--- wp-content/themes/make-experiences/single-espresso_people.php
+++ wp-content/themes/make-experiences/single-espresso_people.php
@@ -17,7 +17,7 @@ get_header();
 		<main id="main" class="site-main">
 			<div class="host-wrapper">
 				<div class="host-photo">
-					<?php get_the_post_thumbnail(); ?>	
+					<?php echo get_the_post_thumbnail(); ?>	
 				</div>
 				
 				<div class="host-meta">
@@ -61,14 +61,61 @@ get_header();
 						<h3>Events <?php echo get_the_title(); ?> is involved with:</h3>
 						<?php foreach ( $events as $type => $event ) : ?>
 							<div class="eea-people-addon-people-type-container">
-								<h4 class="eea-people-addon-people-type-label"><?php echo $type; ?></h4>
-								<ul class="eea-people-addon-event-list-ul">
-									<?php foreach ( $event as $evt ) : ?>
-										<li>
-											<a class="eea-people-addon-link-to-event" href="<?php echo get_permalink( $evt->ID() ); ?>" title="<?php printf( __('Click here to view more info about %s', 'event_espresso' ), $evt->name() ); ?>"><span class="eea-people-addon-event-name"><?php echo $evt->name(); ?></span></a>
-										</li>
-									<?php endforeach; ?>
-								</ul>
+								<?php /* <h4 class="eea-people-addon-people-type-label"><?php echo $type; ?></h4> */ ?>
+								<div class="events-list">
+									<?php foreach ( $event as $evt ) {
+										$date = $evt->first_datetime(); 
+										$dateFormat = date('D <\b/>j<\/b/>', strtotime($date->start_date()));
+										$startime = date('F j, Y @ g:i a', strtotime($date->start_time()));
+										$endime = date('g:i a', strtotime($date->end_time()));
+										// grab array of EE_Ticket objects for event
+										$tickets = EEH_Event_View::event_tickets_available( $evt->ID() );
+										if ( is_array( $tickets ) && count($tickets) > 1 ) {
+											foreach($tickets as  $ticket => $element) {
+												$tickets[$ticket] = preg_replace('/<span[^>]*>([\s\S]*?)<\/span[^>]*>/', '', $tickets[$ticket]->pretty_price());
+											}
+										}
+										sort($tickets);
+										$ticket_price = 'Tickets Not Available';
+										if ( is_array( $tickets ) && count($tickets) > 1 ) {
+											foreach($tickets as $ticket => $element) {
+												reset($tickets);
+												if ($ticket === key($tickets))
+													$ticket_price = $tickets[$ticket];
+
+												end($tickets);
+												if ($ticket === key($tickets) && $tickets[$ticket] != $ticket_price)
+													$ticket_price .= "-" . $tickets[$ticket];
+											}
+										} else if (count($tickets) > 0) {
+											$ticket_price = $tickets[0];
+										}
+										$return = '<article id="post-' . $evt->ID() . '" '. esc_attr( implode( ' ', get_post_class() ) )  .'>
+													 <div class="event-truncated-date">' . $dateFormat . '</div>
+													 <div class="event-image">
+													   <a href="' . get_permalink() . '">
+														 <img src="' . get_the_post_thumbnail_url( $evt->ID(), 'thumbnail' ) . '" />
+													   </a>
+													 </div>
+													 <div class="event-info">
+													   <div class="event-date">'. $startime . ' - ' . $endime . '</div>
+													   <h3 class="event-title">
+														 <a href="' . get_permalink($evt->ID()) . '">' . get_the_title($evt->ID()) . '</a>
+													   </h3>
+													   <div class="event-description">' . get_field('short_description', $evt->ID()) . '</div>
+													   <div class="event-prices">';
+															if($ticket_price != 'Tickets Not Available') {
+																$return .= '<a href="' . get_permalink($evt->ID()) . '" class="btn universal-btn">Get Tickets</a>';
+															}
+															$return .= $ticket_price . 
+													  '</div>
+													 </div>
+												   </article>';
+
+										echo $return;
+
+									} ?>
+								</div>
 							</div>
 						<?php endforeach; ?>
 						<?php endif; ?>
