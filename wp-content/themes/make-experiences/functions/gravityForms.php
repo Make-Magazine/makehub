486957932ce1b23b5d8a76bb04a882aba65807b5
Rio Roth-Barrerio <rio@makermedia.com>
2021-04-10 20:57:58 -0700

    fix for the date and time inputs

--- wp-content/themes/make-experiences/functions/gravityForms.php
+++ wp-content/themes/make-experiences/functions/gravityForms.php
@@ -12,9 +12,6 @@ add_filter('gform_column_input_content_10_11_3', 'set_time_field_type', 10, 6);
 
 //reformat field as date type
 function set_date_field_type($input, $input_info, $field, $text, $value, $form_id) {
-    if(is_admin()){
-        return $input;
-    }
     //build field name, must match List field syntax to be processed correctly
     $input_field_name = 'input_' . $field->id . '[]';
     $input_field_id = 'input_' . $form_id . '_' . $field->id;
@@ -27,9 +24,6 @@ function set_date_field_type($input, $input_info, $field, $text, $value, $form_i
 
 //reformat field as time type
 function set_time_field_type($input, $input_info, $field, $text, $value, $form_id) {
-    if(is_admin()){
-        return $input;
-    }
     $tabindex = GFCommon::get_tabindex();
     $input_field_name = 'input_' . $field->id . '[]';
     $input_field_id = $field->id . "_" . str_replace(" ", "_", strtolower($text));
