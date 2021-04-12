b3ee94fc344feabf415b553bd9cb4b58bbbd0dde
Alicia Williams <alicia@make.co>
2021-04-12 13:53:02 -0600

    adding siana and dan into staging emails

--- wp-content/themes/make-experiences/functions/stagingNotification.php
+++ wp-content/themes/make-experiences/functions/stagingNotification.php
@@ -7,7 +7,7 @@ function change_email_for_wp($email) {
    $homeurl = get_home_url();
    //check for our stage and dev sites
    if(strpos($homeurl,'wpengine.com')!==false){     
-       $email['to'] = 'webmaster@make.co';
+       $email['to'] = 'webmaster@make.co, dan@make.co, siana@make.co';
        $email['subject'] = 'Stage/Dev Redirect Email - '.$email['subject'];              
    }
    
