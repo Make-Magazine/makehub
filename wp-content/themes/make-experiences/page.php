7efad7158e77fda2915bde9a8418c3d47e06f69b
Alicia Williams <alicia@make.co>
2021-04-21 12:24:07 -0600

    remove the multiple 'Submit Event' on the page

--- wp-content/themes/make-experiences/page.php
+++ wp-content/themes/make-experiences/page.php
@@ -11,48 +11,48 @@
  *
  * @package BuddyBoss_Theme
  */
-
 get_header();
 ?>
 
-    <div id="primary" class="content-area bb-grid-cell">
-        <main id="main" class="site-main">
+<div id="primary" class="content-area bb-grid-cell">
+    <main id="main" class="site-main">
 
-			<?php if ( have_posts() ) :
+        <?php
+        if (have_posts()) :
 
-				do_action( THEME_HOOK_PREFIX . '_template_parts_content_top' );
+            do_action(THEME_HOOK_PREFIX . '_template_parts_content_top');
 
-				while ( have_posts() ) :
-					the_post();
-					// Are there custom panels to display?
-					  if( have_rows('content_panels')) {
-						 // loop through the rows of data      
-						 echo '<div class="customPanels">';
-							while ( have_rows('content_panels') ) {
-								the_row();
-								$row_layout = get_row_layout();           
-								echo dispLayout($row_layout);
-							}
-						 echo '</div>';
-					  }
+            //while (have_posts()) :
+                the_post();              
+                // Are there custom panels to display?
+                if (have_rows('content_panels')) {
+                    // loop through the rows of data      
+                    echo '<div class="customPanels">';
+                    while (have_rows('content_panels')) {
+                        the_row();
+                        $row_layout = get_row_layout();
+                        echo dispLayout($row_layout);
+                    }
+                    echo '</div>';
+                }
 
-					do_action( THEME_HOOK_PREFIX . '_single_template_part_content', 'page' );
+                do_action(THEME_HOOK_PREFIX . '_single_template_part_content', 'page');
 
-				endwhile; // End of the loop.
-			else :
-				get_template_part( 'template-parts/content', 'none' );
-				?>
+            //endwhile; // End of the loop.
+        else :
+            get_template_part('template-parts/content', 'none');
+            ?>
 
-			<?php endif; ?>
+        <?php endif; ?>
 
-        </main><!-- #main -->
-    </div><!-- #primary -->
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
-if ( is_search() ) {
-	get_sidebar( 'search' );
+if (is_search()) {
+    get_sidebar('search');
 } else {
-	get_sidebar( 'page' );
+    get_sidebar('page');
 }
 ?>
 
