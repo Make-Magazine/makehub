8d4487cf201363d0c491b2ff203150c4826804fd
Rio Roth-Barrerio <rio@makermedia.com>
2021-04-28 13:15:16 -0700

    woo commerce and facilitators box, regenerate thumbnails plugin

--- wp-content/themes/make-experiences/archive-product.php
+++ wp-content/themes/make-experiences/archive-product.php
@@ -28,10 +28,22 @@ get_header( 'shop' );
  */
 do_action( 'woocommerce_before_main_content' );
 
+$page_title = woocommerce_page_title( false );
+
+switch ($page_title) {
+    case "Experiences":
+        $page_title = "Current Offerings";
+        break;
+    case "Shop":
+        $page_title = "Custom Maker Experiences";
+        break;
+}
+
+
 ?>
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+		<h1 class="woocommerce-products-header__title page-title"><?php echo $page_title; ?></h1>
 	<?php endif; ?>
 
 	<?php
