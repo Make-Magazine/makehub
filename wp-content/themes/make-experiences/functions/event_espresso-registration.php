5ddb1f54c51a0007276a42f406064040002c03c6
Rio Roth-Barrerio <rio@makermedia.com>
2021-05-17 14:50:44 -0700

    welcome email colon and link fixes

--- wp-content/themes/make-experiences/functions/event_espresso-registration.php
+++ wp-content/themes/make-experiences/functions/event_espresso-registration.php
@@ -38,7 +38,7 @@ function attendee_approved( $registration) {
 
 		$subject = 'Welcome to Maker Campus on Make: Community.';
 		$my_groups = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/members/me/groups/";
-		$message = 'You have been given a free membership to Make: Community as part of your Maker Campus event ticket purchase.  Please login to access Make Community and your <a href="'. $my_groups .'">Maker Campus event groups</a>. <br /><br />
+		$message = 'You have been given a free membership to Make: Community as part of your Maker Campus event ticket purchase.  Please login to access Make: Community and your Maker Campus <a href="'. $my_groups .'">event groups</a>. <br /><br />
 <b>Username:</b> ' . $username . '<br />
 <b>Password:</b> ' . $random_password;
 		$headers = array('Content-Type: text/html; charset=ISO-8859-1','From: Make: Community <make@make.co>');
