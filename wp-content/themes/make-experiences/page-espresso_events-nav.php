4717dc6de188e3ee44c4b03d960e1221fff235ff
Alicia Williams <alicia@make.co>
2021-05-21 13:14:35 -0600

    fixing link

--- wp-content/themes/make-experiences/page-espresso_events-nav.php
+++ wp-content/themes/make-experiences/page-espresso_events-nav.php
@@ -13,7 +13,7 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 				<div class="event-view-btns">
 					<a href="/maker-campus" title="Grid View"><i class="fas fa-th-large"></i></a>
-					<a href="/maker-campus/event-list" title="List View"><i class="fas fa-th-list"></i></a>
+					<a href="/maker-campus/events-list" title="List View"><i class="fas fa-th-list"></i></a>
 				</div>
 		<?php
 			do_action( THEME_HOOK_PREFIX . '_template_parts_content_top' );
