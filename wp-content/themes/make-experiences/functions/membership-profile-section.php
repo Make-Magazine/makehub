f653f542cc2a8f05470255d02d96f163cffbae8c
Rio Roth-Barrerio <rio@makermedia.com>
2021-06-01 13:58:31 -0700

    don't show the upgrade button if they already have digital access

--- wp-content/themes/make-experiences/functions/membership-profile-section.php
+++ wp-content/themes/make-experiences/functions/membership-profile-section.php
@@ -8,7 +8,7 @@ function profile_tab_membership_infoname() {
     global $bp;
     $user_id = bp_displayed_user_id();
     $type = bp_get_member_type(bp_displayed_user_id());
-    if ($user_id != 0 && wp_get_current_user()->ID == $user_id ) {
+    if ($user_id != 0 && wp_get_current_user()->ID == $user_id) {
         bp_core_new_nav_item(array(
             'name' => 'Membership',
             'slug' => 'membership',
@@ -41,32 +41,38 @@ function membership_info_content() {
     //get the users email
     $user_info = get_userdata($user_id);
     $user_email = $user_info->user_email;
+    require_once(get_stylesheet_directory() . '/vendor/stripe/stripe-php/init.php');
+    \Stripe\Stripe::setApiKey('sk_live_fx1xtpmDg3BUWIxZwKVfZugt');
+    $customer = \Stripe\Customer::all(["email" => $user_email]);
+    $customerID = $customer->data[0]['id'];
 
-	require_once(get_stylesheet_directory() . '/vendor/stripe/stripe-php/init.php');
-	\Stripe\Stripe::setApiKey('sk_live_fx1xtpmDg3BUWIxZwKVfZugt');
-	$customer = \Stripe\Customer::all(["email" => $user_email]);
-	$customerID = $customer->data[0]['id'];
-	
-	echo '<div class="membership-tab-wrapper">';
-		echo '<h1>Make: Membership Details</h1>';	
-		//var_dump(Ihc_Db::get_user_levels($user_id, true));
-		echo do_shortcode("[ihc-list-user-levels exclude_expire=true]");
-		if( !is_null($customerID) ) { // if customer exists in stripe
-			$session = \Stripe\BillingPortal\Session::create([
-			  'customer' => $customerID,
-			  'return_url' => 'https://' . $_SERVER['SERVER_NAME'] . '/members/' . $user_info->display_name . "/membership",
-			]);
-			echo '<a href="' . $session->url . '" class="btn universal-btn" id="manage-membership-btn" target="_blank">Update Payment information</a>';
-			if (!class_exists('ihcAccountPage')){
-				require_once IHC_PATH . 'classes/ihcAccountPage.class.php';
-			}
-			$obj = new ihcAccountPage($attr);
-			echo $obj->print_page("orders");
-		} else {
-			echo '<p>Upgrade your subscription for digital Make: Magazine access and exclusive videos.</p>';
-			echo '<a href="/join" class="btn universal-btn">Upgrade your Subscription</a>';
-		}
-		
-	echo '</div>';
+    echo '<div class="membership-tab-wrapper">';
+    echo '<h1>Make: Membership Details</h1>';
+    
+    $hasDigitalAccess = false;
+    $levels = Ihc_Db::get_user_levels($user_id, true);
+    foreach($levels as $level) {
+        if($level['level_slug'] != "community") {
+            $hasDigitalAccess = true;
+        }
+    }
+    echo do_shortcode("[ihc-list-user-levels exclude_expire=true]");
+    if (!is_null($customerID)) { // if customer exists in stripe
+        $session = \Stripe\BillingPortal\Session::create([
+            'customer' => $customerID,
+            'return_url' => 'https://' . $_SERVER['SERVER_NAME'] . '/members/' . $user_info->display_name . "/membership",
+        ]);
+        echo '<a href="' . $session->url . '" class="btn universal-btn" id="manage-membership-btn" target="_blank">Update Payment information</a>';
+        if (!class_exists('ihcAccountPage')) {
+            require_once IHC_PATH . 'classes/ihcAccountPage.class.php';
+        }
+        $obj = new ihcAccountPage($attr);
+        echo $obj->print_page("orders");
+    }
+    if($hasDigitalAccess == false) {
+        echo '<p>Upgrade your subscription for digital Make: Magazine access and exclusive videos.</p>';
+        echo '<a href="/join" class="btn universal-btn">Upgrade your Subscription</a>';
+    }
+
+    echo '</div>';
 }
-
