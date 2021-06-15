318fd822eae28a25c7074d4a984d76243a7e5f89
Rio Roth-Barrerio <rio@makermedia.com>
2021-06-15 09:20:48 -0700

    add back in the event espresso functions to maker-camp theme

---
+++ wp-content/themes/make-makercamp/functions/event_espresso-registration.php
@@ -0,0 +1,87 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+add_filter( 'AHEE__EE_Registration__set_status__to_approved', 'attendee_approved', 4 );
+//attendee registration approved
+function attendee_approved( $registration) {            
+    $attendeeID = $registration->attendee_ID();
+    $eventID = $registration->event_ID();
+    $attendee = EEM_Attendee::instance()->get_one_by_ID($attendeeID);
+    
+    //get the user information for the attendee
+    $attendeeEmail = $attendee->email();
+    $user = get_user_by('email', $attendeeEmail);
+    
+    if(!$user) {
+        //create a user
+        $username = strstr($attendeeEmail, '@', true); //first try username being the first part of the email
+        if(username_exists( $username )){  //username exists try something else
+            $count=1;
+            $exists = true;
+            while($exists){
+                $username = $username.$count;
+                if(!username_exists($username)){
+                    $exists = false;
+                }
+                $count++;
+            }
+        }
+        
+        //generate random password, create user, send email        
+        $random_password = wp_generate_password( 12, false );
+        $user_id = wp_create_user( $username, $random_password, $attendeeEmail );
+		update_user_meta( $user_id, 'first_name', $attendee->fname() );
+		update_user_meta( $user_id, 'last_name', $attendee->lname() );
+
+		$subject = 'Welcome to Maker Campus on Make: Community.';
+		$my_groups = 'https://devmakehub.wpengine.com/wp-login.php?redirect_to='.(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/members/me/groups/";
+		$message = 'Hello ' . $attendee->fname().', <br /><br />Thank you for registering for an upcoming Maker Campus program.  Included with the event ticket is a free membership to Make:Community. This is where you will find the event information, resources and community.  Please login to access Make Community and your Maker Campus <a href="'. $my_groups .'">event groups</a>. <br /><br />
+<b>Username:</b> ' . $username . '<br />
+<b>Temporary Password:</b> ' . $random_password;
+		$headers = array('Content-Type: text/html; charset=ISO-8859-1','From: Make: Community <make@make.co>');
+		wp_mail( $attendeeEmail, $subject, $message, $headers );
+
+    }else{        
+        $user_id = $user->ID;
+    }
+    
+    //add wp_EE_Attendee_ID usermeta
+    //is wp_EE_Attendee_ID set?
+    $havemeta = get_user_meta($user_id, 'wp_EE_Attendee_ID', true); 
+    if(!$havemeta){
+        $attendeeID = $attendee->get('ATT_ID');
+        add_user_meta($user_id,'wp_EE_Attendee_ID',$attendeeID);
+    }
+        
+    // give them a free membership    
+    $result = ihc_do_complete_level_assign_from_ap($user_id, 14, 0, 0);
+    
+    //add them to the event group    
+    $group_id = get_field('group_id', $eventID);
+    
+    groups_join_group( $group_id, $user_id);
+    
+    return $registration;
+}
+
+add_filter( 'FHEE__thank_you_page_overview_template__order_conf_desc', 'confirmation_page_text', 4 );
+function confirmation_page_text($order_conf_desc){    
+    $order_conf_desc = 'Your registration has been successfully processed. '.                        
+                        'As part of the Maker Campus experience, all registered attendees have been given a free membership to Make: Community. '.
+                        'This membership provides attendees with a central hub for the workshop; material list, online webinar access, group access to connect with the facilitator, attendees, and more!  Make: Community is a great place to connect with others and find making activities online and at your local makerspace.<br/><br/>'.                      
+                        'Attendees, check your email for your registration confirmation and login instructions to access your event information and benefit from the full Maker Campus experience. '.
+                        'Click the button below to view / download / print a full description of your purchases and registration information.<br/><br   />';
+    $order_conf_desc .=   (is_user_logged_in()?'<a class="ee-button ee-roundish indented-text big-text" href="/members/me/groups/">View Event Group</a>':'');
+    return $order_conf_desc;
+}
+
+add_filter('FHEE__EED_Multi_Event_Registration__return_to_events_list_btn_txt','change_return_to_event_text',1);
+function change_return_to_event_text($text){
+    $text = 'Return to Event';
+    return $text;
+}
---
+++ wp-content/themes/make-makercamp/functions/event_espresso.php
@@ -0,0 +1,92 @@
+<?php
+
+// Overwrite the event dates display
+function espresso_list_of_event_dates( $EVT_ID = 0, $date_format = '', $time_format = '', $echo = TRUE, $show_expired = NULL, $format = TRUE,  $limit = NULL ) {
+	$date_format = ! empty( $date_format ) ? $date_format : get_option( 'date_format' );
+	$time_format = ! empty( $time_format ) ? $time_format : get_option( 'time_format' );
+	$date_format = apply_filters( 'FHEE__espresso_list_of_event_dates__date_format', $date_format );
+	$time_format = apply_filters( 'FHEE__espresso_list_of_event_dates__time_format', $time_format );
+	$datetimes = EEH_Event_View::get_all_date_obj( $EVT_ID, $show_expired, FALSE, $limit );
+	if ( ! $format ) {
+		return apply_filters( 'FHEE__espresso_list_of_event_dates__datetimes', $datetimes );
+	}
+	//d( $datetimes );
+	if ( is_array( $datetimes ) && ! empty( $datetimes )) {
+		global $post;
+		$html = $format ? '<ul id="ee-event-datetimes-ul-' . $post->ID . '" class="ee-event-datetimes-ul ee-clearfix">' : '';
+		foreach ( $datetimes as $datetime ) {
+			if ( $datetime instanceof EE_Datetime ) {
+				$html .= '<li id="ee-event-datetimes-li-' . $datetime->ID();
+				$html .= '" class="ee-event-datetimes-li ee-event-datetimes-li-' . $datetime->get_active_status() . '">';
+				$datetime_name = $datetime->name();
+				$html .= ! empty( $datetime_name ) ? '<h3>' . $datetime_name . '</h3>' : '';
+				$html .= '<span class="dashicons dashicons-calendar"></span><span class="ee-event-datetimes-li-daterange">' . $datetime->date_range( $date_format ) . '</span>';
+				$html .= '<span class="dashicons dashicons-clock"></span><span class="ee-event-datetimes-li-timerange">' . $datetime->time_range( $time_format ) . ' Pacific</span>';
+				$datetime_description = $datetime->description();
+				$html .= ! empty( $datetime_description ) ? ' - ' . $datetime_description : '';
+				//$html = apply_filters( 'FHEE__espresso_list_of_event_dates__datetime_html', $html, $datetime );
+				$html .= '</li>';
+			}
+		}
+		$html .= $format ? '</ul>' : '';
+	} else {
+		$html = $format ?  '<p><span class="dashicons dashicons-marker pink-text"></span>' . esc_html__( 'There are no upcoming dates for this event.', 'event_espresso' ) . '</p><br/>' : '';
+	}
+	if ( $echo ) {
+		echo $html;
+		return '';
+	}
+	return $html;
+}
+
+function event_ticket_prices($post) {
+	// grab array of EE_Ticket objects for event
+	$tickets = EEH_Event_View::event_tickets_available( $post->ID() );
+	$formatter = new NumberFormatter('en_US', NumberFormatter::CURRENCY);
+	if ( is_array( $tickets ) && count($tickets) > 1 ) {
+		foreach($tickets as $ticket => $element) {
+			$tickets[$ticket] = $tickets[$ticket]->ticket_price();
+		}
+	}
+	sort($tickets, SORT_NUMERIC);
+	$ticket_price = 'Tickets Not Available';
+	if ( is_array( $tickets ) && count($tickets) > 1 ) {
+		foreach($tickets as $ticket => $element) {
+			reset($tickets);
+			if ($ticket === key($tickets))
+				$ticket_price = $formatter->formatCurrency($tickets[$ticket], 'USD');
+				if(trim($ticket_price) == "$0.00"){
+					$ticket_price = 'FREE';
+				}
+			end($tickets);
+			if ( $ticket === key($tickets) && $formatter->formatCurrency($tickets[$ticket], 'USD') != $ticket_price ) {
+				$ticket_price .= " - " . $formatter->formatCurrency($tickets[$ticket], 'USD');
+			}
+		}
+	} else if (count($tickets) > 0) {
+		$ticket_price = $formatter->formatCurrency($tickets[0]->ticket_price(), 'USD');
+		if(trim($ticket_price) == "$0.00"){
+			$ticket_price = 'FREE';
+		}
+	}
+	return $ticket_price;
+}
+
+function events_sidebar() {
+
+    register_sidebar( array(
+        'id'            => 'event_grid_sidebar',
+        'name'          => 'Event Grid Sidebar',
+    ) );
+
+}
+add_action( 'widgets_init', 'events_sidebar' );
+
+//default the admin menu link to sort by newest event first
+add_action ( 'admin_menu', 'ee_filter_ee_events_orderyby_datetime', 99 );
+function ee_filter_ee_events_orderyby_datetime() {
+    // call global submenu item
+    global $submenu;
+    // edit main link for events
+    $submenu['espresso_events'][0][2] = 'admin.php?page=espresso_events&orderby=Datetime.DTT_EVT_start&order=desc';
+}
