324d0325f427929d6747221be006edaffdfd2781
Rio Roth-Barrerio <rio@makermedia.com>
2021-06-23 10:07:55 -0700

    remove courses from universal nav profile dropdown, change to and cc for Redirect emails on dev or local sites

--- wp-content/themes/make-experiences/functions/stagingNotification.php
+++ wp-content/themes/make-experiences/functions/stagingNotification.php
@@ -22,7 +22,16 @@ function change_email_for_wp($email) {
 
         $email['subject'] = 'Redirect Email sent to ' . $email['to'] . ' - ' . $email['subject'];
         //$email['to'] = implode(",", $newTo);
-        $email['to'] = 'webmaster@make.co, dan@make.co, siana@make.co';
+		$email['headers'] = array(
+  			'Cc: ',
+		);
+		if(strpos($homeurl, 'makehub.local') !== false) {
+			$email['to'] = 'rio@make.co';
+		} else if(strpos($homeurl, 'makehub.test') !== false) {
+			$email['to'] = 'alicia@make.co';
+		} else {
+        	$email['to'] = 'webmaster@make.co, dan@make.co, siana@make.co';
+		}
     }
 
     return ($email);
