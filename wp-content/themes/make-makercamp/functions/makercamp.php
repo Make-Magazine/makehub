d11acd5c42ebbb3c7989c02ad6cfcfad79bdcde0
Alicia Williams <alicia@make.co>
2021-06-25 12:52:25 -0600

    add materials section and author section to print page

--- wp-content/themes/make-makercamp/functions/makercamp.php
+++ wp-content/themes/make-makercamp/functions/makercamp.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,10 +8,21 @@
 function get_lesson_output($lesson_id, $course_id) {
     global $post;
     do_action(THEME_HOOK_PREFIX . '_template_parts_content_top');
+    $course_settings = learndash_get_setting($lesson_id);
+
+    $lesson_materials = '';
+    if (( 'on' === $course_settings['lesson_materials_enabled'] ) && (!empty($course_settings['lesson_materials']) )) {
+        $lesson_materials = wp_specialchars_decode($course_settings['lesson_materials'], ENT_QUOTES);
+        if (!empty($lesson_materials)) {
+            $lesson_materials = do_shortcode($lesson_materials);
+        }
+    }
     ?>
     <div id="primary" class="content-area bb-grid-cell">
         <main id="main" class="site-main">
             <div style="text-align:center"><img src="<?php echo get_stylesheet_directory_uri() . '/images/makercamp-logo.png'; ?> " /></div>
+
+            <div style="float:left;width:70%">
                 <?php
                 //print out lesson content                    
                 $post = get_post($lesson_id, OBJECT); //set post info                                       
@@ -25,6 +35,12 @@ function get_lesson_output($lesson_id, $course_id) {
                     get_template_part('template-parts/content', 'sfwd');
                 }
                 ?>
+            </div>
+            <div style="float:left;width:30%">
+                <?php echo $lesson_materials; ?>    
+            </div>                        
+
+
         </main>
     </div>
     <?php
--- wp-content/themes/make-makercamp/project-print-template.php
+++ wp-content/themes/make-makercamp/project-print-template.php
@@ -10,24 +10,27 @@
             var logoutURL = '<?php echo wp_logout_url(home_url()); ?>';
         </script>
         <style>
-            #wpadminbar, .ldms-message-tab,a.nt-note-tab {display:none}
-			@media print {
-				.elementor-column.elementor-col-50 {
-					width: 50% !important;
-				}
-				.elementor-column.elementor-col-33 {
-					width: 33.333% !important;
-				}
-				.elementor-social-icons-wrapper, .elementor-social-icon, .elementor-social-icon svg {
-					display: none;
-					visibility: hidden;
-					height: 0p
-				}
-				* {
-					-webkit-print-color-adjust: exact !important;   /* Chrome, Safari */
-					color-adjust: exact !important;                 /*Firefox*/
-				}
-			}
+            #wpadminbar, .ldms-message-tab,a.nt-note-tab, .ldfc-favorite-button {display:none}
+            .container, .elementor-section.elementor-section-boxed > .elementor-container {width:100% !important; max-width: 100% !important}
+            @media print {
+                .container, .elementor-section.elementor-section-boxed > .elementor-container {width:100% !important; max-width: 100% !important}
+                .elementor-column.elementor-col-50 {
+                    //width: 50% !important;
+                }
+                .elementor-column.elementor-col-33 {
+                    //width: 33.333% !important;
+                }
+                .elementor-social-icons-wrapper, .elementor-social-icon, .elementor-social-icon svg {
+                    display: none;
+                    visibility: hidden;
+                    height: 0p
+                }
+                * {
+                    -webkit-print-color-adjust: exact !important;   /* Chrome, Safari */
+                    color-adjust: exact !important;                 /*Firefox*/
+                }
+            }
+
         </style>
 
         <?php wp_head(); ?>
@@ -35,15 +38,10 @@
 
     <body <?php body_class(); ?>>
         <?php
-        $lesson_id = '';
-        $course_id = '';
+        $lesson_id = (isset($_GET['lesson'])?$_GET['lesson']:'');
+        $course_id = (isset($_GET['course'])?$_GET['course']:'');;
 
         //is this for a lesson or a course?
-        if (isset($_GET['lesson'])) {
-            $lesson_id = $_GET['lesson'];
-        } elseif (isset($_GET['course'])) {
-            $course_id = $_GET['course'];
-        }
         if ($course_id != '') {
             $lessons = learndash_course_get_lessons($course_id);
             foreach ($lessons as $lesson) {
@@ -55,21 +53,34 @@
             get_lesson_output($lesson_id, $course_id);
         }
         ?>
+     
         <div class="elementor-section elementor-section-boxed">
             <section class="elementor-container">
-                <p style="width:100%"><strong>Please Note</strong></p>
-                <p>Your safety is your own responsibility, including proper use of equipment and safety gear, and determining whether you have adequate skill and experience. Power tools, electricity, and other resources used for these projects are dangerous, unless used properly and with adequate precautions, including safety gear and adult supervision. Some illustrative photos do not depict safety precautions or equipment, in order to show the project steps more clearly. Use of the instructions and suggestions found in Maker Camp is at your own risk. Maker Media, Inc., disclaims all responsibility for any resulting damage, injury, or expense.</p>
+                <div class="elementor-widget-container">
+                    <?php
+                // Author section
+                global $post;
+                $course_post = learndash_get_setting($post, 'course');
+                $course_data = get_post($course_post);
+                $author_id = $course_data->post_author;
+                learndash_get_template_part('template-course-author.php', array(
+                    'user_id' => $author_id
+                        ), true);
+                ?>
+                    <p style="width:100%"><strong>Please Note</strong></p>
+                    <p>Your safety is your own responsibility, including proper use of equipment and safety gear, and determining whether you have adequate skill and experience. Power tools, electricity, and other resources used for these projects are dangerous, unless used properly and with adequate precautions, including safety gear and adult supervision. Some illustrative photos do not depict safety precautions or equipment, in order to show the project steps more clearly. Use of the instructions and suggestions found in Maker Camp is at your own risk. Maker Media, Inc., disclaims all responsibility for any resulting damage, injury, or expense.</p>
+                </div>
             </section>
         </div>
 
         <?php wp_footer(); ?>
 
         <script>
-            jQuery(window).on("load", function() {
-				 jQuery(document).scrollTop(jQuery(document).height());
-				 window.print();
+            jQuery(window).on("load", function () {
+               jQuery(document).scrollTop(jQuery(document).height());
+               window.print();
             });
         </script>
-		
+
     </body>
 </html>
