80a33c2d246737764246d4ae1186e616fe649882
Rio Roth-Barrerio <rio@makermedia.com>
2021-07-16 14:55:34 -0700

    redirect logins on homepage to dashboard

--- wp-content/themes/make-experiences/js/site/global.js
+++ wp-content/themes/make-experiences/js/site/global.js
@@ -5,9 +5,11 @@ function isValidEmailAddress(emailAddress) {
 
 jQuery(document).ready(function () {
 	jQuery('#LoginBtn').attr("href", function(i, href) {
+		var redirect_url = window.location.protocol + "//" + window.location.host + "/members/me/";
 		if(window.location.pathname == "/join/") { // if they logged in from the join page, send them to the referrer if it exists
-			var redirect_url = window.location.protocol + "//" + window.location.host + "/members/me/";
 			if(document.referrer && document.referrer != "") { redirect_url = document.referrer; }
+			return href + '?redirect_to=' + redirect_url;
+		} else if(window.location.pathname == "/") { // if on homepage, redirect to dashboard
 			return href + '?redirect_to=' + redirect_url;
 		} else { // otherwise send them to where they logged in from
 			return href + '?redirect_to=' + window.location.href;
