6c3c301fca1428e857f8e78b7297ab11391cf858
Rio Roth-Barreiro <rio@make.co>
2021-09-30 10:19:35 -0700

    show most recent video gallery first

--- wp-content/themes/make-experiences/page-videogallery.php
+++ wp-content/themes/make-experiences/page-videogallery.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
  /**
  * Template Name: Video Gallery
  *
  * @version 1.0
  */
-   
+
 if (!is_user_logged_in()) { // Make sure membership is active
 	auth_redirect();
 }
-get_header(); 
+get_header();
 $querystr = "
-    SELECT $wpdb->vimeography_gallery.* 
-    FROM $wpdb->vimeography_gallery
+    SELECT $wpdb->vimeography_gallery.*
+    FROM $wpdb->vimeography_gallery ORDER BY id DESC
 ";
 $galleryInfo = $wpdb->get_results($querystr, OBJECT);
 // get the first value, since the first gallery won't always have id = 1
@@ -28,7 +28,7 @@ if(isset($_GET['gallery'])){
 }
 $pageUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-?>  
+?>
 
 <div class="vimeography-header">
    <h1>Welcome to the members-only video playlists.</h1>
@@ -46,14 +46,14 @@ $pageUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
    </form>
 </div>
 
-<div class="container page-content video-gallery"> 
-    <?php 
+<div class="container page-content video-gallery">
+    <?php echo($galleryID);
     if($galleryID != 'youtube') {
-        echo(do_shortcode('[vimeography id="' . $galleryID . '"]')); 
-    } else { ?> 
+        echo(do_shortcode('[vimeography id="' . $galleryID . '"]'));
+    } else { ?>
         <?php echo do_shortcode('[embedyt]https://www.youtube.com/embed?listType=playlist&amp;list=UUhtY6O8Ahw2cz05PS2GhUbg&amp;layout=gallery[/embedyt]'); ?>
         <h2 class="text-center"><a href="https://www.youtube.com/c/MAKE/featured" target="_blank">See more videos from our Youtube Channel</a></h2>
-    <?php } ?> 
+    <?php } ?>
 </div><!-- end .page-content -->
 
 <?php get_footer(); ?>
