b0ec4bf6f8660084e9fdf4fa29373c5567a300d5
Rio Roth-Barreiro <rio@make.co>
2021-11-09 16:10:11 -0800

    revise staging notifications

--- wp-content/themes/make-experiences/functions/stagingNotification.php
+++ wp-content/themes/make-experiences/functions/stagingNotification.php
@@ -21,14 +21,15 @@ function change_email_for_wp($email) {
         }*/
 
         $email['subject'] = 'Redirect Email sent to ' . $email['to'] . ' - ' . $email['subject'];
+		$email['headers'] .= 'Cc: ""' . "\r\n";
 
-    		if(strpos($homeurl, 'makehub.local') !== false) {
-    			$email['to'] = 'rio@make.co';
-    		} else if(strpos($homeurl, 'makehub.test') !== false) {
-    			$email['to'] = 'alicia@make.co';
-    		} else {
-          $email['to'] = 'webmaster@make.co, dan@make.co, siana@make.co';
-    		}
+		if(strpos($homeurl, 'makehub.local') !== false) {
+			$email['to'] = 'rio@make.co';
+		} else if(strpos($homeurl, 'makehub.test') !== false) {
+			$email['to'] = 'alicia@make.co';
+		} else {
+      		$email['to'] = 'webmaster@make.co, dan@make.co, siana@make.co';
+		}
     }
 
     return ($email);
