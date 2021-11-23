a5cad29f90ee1a14edfa2d37009f6c081d183665
Rio Roth-Barreiro <rio@make.co>
2021-11-23 11:48:26 -0800

    add main category to body class for courses and add style to Education courses based on that

--- wp-content/themes/make-experiences/functions.php
+++ wp-content/themes/make-experiences/functions.php
@@ -314,6 +314,12 @@ function add_slug_body_class($classes) {
         } else {
             $classes[] = $post->post_type . '-' . str_replace("/", "-", trim($_SERVER['REQUEST_URI'], '/'));
         }
+		if ( $post->post_type == "sfwd-courses" && get_post_primary_category($post->ID, 'ld_course_category')['primary_category']) {
+			$classes[] = 'cat-' . get_post_primary_category($post->ID, 'ld_course_category')['primary_category']->slug;
+		}
+		if ( $post->post_type == "sfwd-lessons" && get_post_primary_category($post->ID, 'ld_lesson_category')['primary_category'] ) {
+			$classes[] = 'cat-' . get_post_primary_category($post->ID, 'ld_lesson_category')['primary_category']->slug;
+		}
         // let's see if your the group owner and what kind of group it is (hidden, private, etc)
         if (bp_is_groups_component()) {
             $classes[] = 'group-' . groups_get_group(array('group_id' => bp_get_current_group_id()))->status;
@@ -384,4 +390,32 @@ function gf_add_entries_link( $wp_admin_bar ) {
 }
 
 add_filter( 'admin_bar_menu', 'gf_add_entries_link', 25 );
+
+// get the main category of a post
+function get_post_primary_category($post_id, $term='category', $return_all_categories=false){
+    $return = array();
+    if (class_exists('WPSEO_Primary_Term')){
+        // Show Primary category by Yoast if it is enabled & set
+        $wpseo_primary_term = new WPSEO_Primary_Term( $term, $post_id );
+        $primary_term = get_term($wpseo_primary_term->get_primary_term());
+        if (!is_wp_error($primary_term)){
+            $return['primary_category'] = $primary_term;
+        }
+    }
+    if (empty($return['primary_category']) || $return_all_categories){
+        $categories_list = get_the_terms($post_id, $term);
+        if (empty($return['primary_category']) && !empty($categories_list)){
+            $return['primary_category'] = $categories_list[0];  //get the first category
+        }
+        if ($return_all_categories){
+            $return['all_categories'] = array();
+            if (!empty($categories_list)){
+                foreach($categories_list as &$category){
+                    $return['all_categories'][] = $category->term_id;
+                }
+            }
+        }
+    }
+    return $return;
+}
 ?>
