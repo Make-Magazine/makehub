314329add8556cd6150c3850878dacafb5909896
Alicia Williams <alicia@make.co>
2021-11-23 20:59:16 -0700

    yoast plugin update

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitba8e84bc949e3944fe551dbda5d51be0::getLoader();
+return ComposerAutoloaderInit0a039d5a0b02f8b13866ea282f5de86c::getLoader();
