9d5815ea0f73acad320f335d0a478de55a8f68bf
Rio Roth-Barreiro <rio@make.co>
2021-12-23 09:43:44 -0800

    add default body classes to makerspaces.make.co to fix fatal error, create default 404 page modeled after makerfaire

---
+++ wp-content/themes/make-makerspaces/404.php
@@ -0,0 +1,51 @@
+
+<?php
+
+/*
+Template Name: 404
+*/
+
+get_header( 'universal' );
+
+?>
+<style>
+   body {
+    background-color: #0a5b79;
+}
+   div.caption404 {
+    border: 14px solid rgba(255,255,255,.4);
+    border-radius: 40px;
+    margin-bottom: 20px;
+    margin-top: 50px;
+    padding: 40px;
+}
+p.headline404 {
+    color: #fff;
+    font-size: 25px;
+}
+p.body404 {
+    color: #fff;
+    font-size: 20px;
+    font-weight: lighter;
+}
+</style>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-8">
+      <div class="caption404">
+        <p class="headline404">This is not the page you're looking for.</p>
+        <p class="body404">
+           It must have moved or mysteriously departed.<br><Br>
+           Take me back to the <a href='https://makerspaces.make.co'>Makerspaces.</a>
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-offset-1 col-sm-offset-4">
+      <img src="<?php echo get_stylesheet_directory_uri(). '/images/404-makey.png' ?>" alt="Makey 404" />
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
