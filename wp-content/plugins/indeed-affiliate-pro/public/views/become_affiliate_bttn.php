702e49bb0d0fac40163fce056852a5fbed5cccb0
Rio Roth-Barreiro <rio@make.co>
2022-02-25 10:52:32 -0800

    remove ultimate membership pro plugins

--- wp-content/plugins/indeed-affiliate-pro/public/views/become_affiliate_bttn.php
+++
@@ -1,9 +0,0 @@
-<?php if ( $data['show_button'] ):?>
-<div class="uap-become-affiliate-wrapp">
-	<button class="uap-become-affiliate-bttn" onclick="uapBecomeAffiliatePublic();"><?php esc_html_e('Become an Affiliate', 'uap');?></button>
-</div>
-<?php else :?>
-		<?php if ( $data['warning_message'] ):?>
-				<p><?php echo $data['warning_message'];?></p>
-		<?php endif;?>
-<?php endif;?>
--- wp-content/plugins/indeed-membership-pro/assets/js/cookies.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-jQuery( document ).on( 'ready', function(){
-    document.cookie = 'ihc_payment=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-});
