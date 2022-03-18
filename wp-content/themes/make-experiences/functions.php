a61de887fba9f4a7c3179373b06b2092eaafb79b
Alicia Williams <alicia@make.co>
2022-03-18 12:14:41 -0600

    a few more changes to allow admins to edit users

--- wp-content/themes/make-experiences/functions.php
+++ wp-content/themes/make-experiences/functions.php
@@ -500,5 +500,26 @@ function mc_admin_users_caps( $caps, $cap, $user_id, $args ){
     }
     return $caps;
 }
-add_filter( 'map_meta_cap', 'mc_admin_users_caps', 10, 4 );
+add_filter( 'map_meta_cap', 'mc_admin_users_caps', 1, 4 );
+remove_all_filters( 'enable_edit_any_user_configuration' );
+add_filter( 'enable_edit_any_user_configuration', '__return_true');
+
+/**
+ * Checks that both the editing user and the user being edited are
+ * members of the blog and prevents the super admin being edited.
+ */
+function mc_edit_permission_check() {
+    global $current_user, $profileuser;
+    $screen = get_current_screen();
+    get_currentuserinfo();
+
+    if( ! is_super_admin( $current_user->ID ) && in_array( $screen->base, array( 'user-edit', 'user-edit-network' ) ) ) { // editing a user profile
+        if ( is_super_admin( $profileuser->ID ) ) { // trying to edit a superadmin while less than a superadmin
+            wp_die( __( 'You do not have permission to edit this user.' ) );
+        } elseif ( ! ( is_user_member_of_blog( $profileuser->ID, get_current_blog_id() ) && is_user_member_of_blog( $current_user->ID, get_current_blog_id() ) )) { // editing user and edited user aren't members of the same blog
+            wp_die( __( 'You do not have permission to edit this user.' ) );
+        }
+    }
+}
+add_filter( 'admin_head', 'mc_edit_permission_check', 1, 4 );
 ?>
