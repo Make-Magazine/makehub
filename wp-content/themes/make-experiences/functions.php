7ff54f4be86bfff54af39f071c56c1077ea4092f
Rio Roth-Barreiro <rio@make.co>
2022-03-23 10:03:54 -0700

    revise adding course/lesson category to body class code to be more readable with less calls

--- wp-content/themes/make-experiences/functions.php
+++ wp-content/themes/make-experiences/functions.php
@@ -365,12 +365,19 @@ function add_slug_body_class($classes) {
         } else {
             $classes[] = $post->post_type . '-' . str_replace("/", "-", trim($_SERVER['REQUEST_URI'], '/'));
         }
-		if ( $post->post_type == "sfwd-courses" && isset( get_post_primary_category( $post->ID, 'ld_course_category')['primary_category'] ) ) {
-			$classes[] = 'cat-' . get_post_primary_category($post->ID, 'ld_course_category')['primary_category']->slug;
+		// For Course and Lessons, check for the Primary category and add it to the body class if found
+		if ( $post->post_type == "sfwd-courses") {
+		      $ld_course_category = get_post_primary_category($post->ID, 'ld_course_category');
+		      if(isset($ld_course_category['primary_category']->slug)) {
+					     $classes[] = 'cat-' . $ld_course_category['primary_category']->slug;
+		      }
+		} else if ( $post->post_type == "sfwd-lessons") {
+		      $ld_lesson_category = get_post_primary_category($post->ID, 'ld_lesson_category');
+		      if(isset($ld_lesson_category['primary_category']->slug)) {
+					   $classes[] = 'cat-' . $ld_lesson_category['primary_category']->slug;
+		      }
 		}
-		if ( $post->post_type == "sfwd-lessons" && isset( get_post_primary_category( $post->ID, 'ld_lesson_category')['primary_category'] ) ) {
-			$classes[] = 'cat-' . get_post_primary_category($post->ID, 'ld_lesson_category')['primary_category']->slug;
-		}
+
         // let's see if your the group owner and what kind of group it is (hidden, private, etc)
         if (bp_is_groups_component()) {
             $classes[] = 'group-' . groups_get_group(array('group_id' => bp_get_current_group_id()))->status;
