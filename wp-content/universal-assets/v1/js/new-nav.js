aa331977d04c9e7626bd969657c061531d7931de
Alicia Williams <alicia@make.co>
2022-04-27 09:45:19 -0600

    more api changes

--- wp-content/universal-assets/v1/js/new-nav.js
+++ wp-content/universal-assets/v1/js/new-nav.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function () {
 
-	    // Change logo based on site
+	  // Change logo based on site
     var site = window.location.hostname
-	var sitename = "Make: Community";
+	  var sitename = "Make: Community";
     switch (site) {
         case "makerfaire.local":
         case "makerfaire.test":
@@ -14,7 +14,7 @@ jQuery(document).ready(function () {
         case "mfairedev.wpengine.com":
             jQuery("h2.site-title a").attr("href", "https://makerfaire.com");
             document.getElementById("navLogo").src = "/wp-content/themes/makerfaire/img/Maker_Faire_Logo.svg";
-			sitename = "Maker Faire";
+			      sitename = "Maker Faire";
             break;
         case "makercamp.local":
         case "makercamp.test":
@@ -30,7 +30,7 @@ jQuery(document).ready(function () {
         case "makercamp.stagemakehub.wpengine.com":
             jQuery("h2.site-title a").attr("href", "https://makercamp.make.co");
             document.getElementById("navLogo").src = "https://makercamp.make.co/wp-content/universal-assets/v1/images/makercamp-logo.png";
-			sitename = "Maker Camp";
+			      sitename = "Maker Camp";
             break;
         case "makezine.test":
         case "makezine.local":
@@ -38,19 +38,19 @@ jQuery(document).ready(function () {
         case "makezine.com":
         case "stage.makezine.com":
         case "dev.makezine.com":
-		case "mzinedev.wpengine.com":
-		case "mzinestage.wpengine.com":
+		    case "mzinedev.wpengine.com":
+        case "mzinestage.wpengine.com":
             jQuery("h2.site-title a").attr("href", "https://makezine.com");
             jQuery("#site-logo .nav-logo").css("height", "30.4px");
             jQuery("#site-logo .nav-logo").css("margin-top", "-5px");
-			sitename = "Make: Magazine";
+			      sitename = "Make: Magazine";
             break;
         case "makerspaces.makehub.test":
         case "makerspaces.makehub.local":
         case "makerspaces.devmakehub.wpengine.com":
         case "makerspaces.stagemakehub.wpengine.com":
         case "makerspaces.make.co":
-			sitename = "Maker Spaces";
+            sitename = "Maker Spaces";
             jQuery("h2.site-title a").attr("href", "https://makerspaces.make.co");
             document.getElementById("navLogo").src = "/wp-content/universal-assets/v1/images/makerspaces-logo.jpg";
             break;
@@ -61,11 +61,11 @@ jQuery(document).ready(function () {
         case "learn.make.co":
             jQuery("h2.site-title a").attr("href", "https://learn.make.co");
             document.getElementById("navLogo").src = "/wp-content/universal-assets/v1/images/make-learning-labs-logo.png";
-			sitename = "Learning Labs";
+			      sitename = "Learning Labs";
             break;
         default:// the default is makehub/make.co
             if (window.location.href.indexOf("makercampus") > -1 || window.location.href.indexOf("maker-campus") > -1) {
-				sitename = "Maker Campus";
+                sitename = "Maker Campus";
                 // except for makercampus which gets it's own logo and subnav items
                 jQuery("h2.site-title a").attr("href", "https://make.co/maker-campus");
                 jQuery("#site-logo .nav-logo").css("margin-top", "-8px");
@@ -84,7 +84,6 @@ jQuery(document).ready(function () {
      */
 
     // Buddyboss code
-
     function conNavArray(arr) {
         if (Array.isArray(arr)) {
             for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
@@ -141,9 +140,7 @@ jQuery(document).ready(function () {
     }
 
     if (typeof (primaryNav) != 'undefined' && primaryNav != null) {
-
         navListOrder();
-
         setTimeout(
                 function () {
                     navListOrder();
@@ -196,6 +193,7 @@ jQuery(document).ready(function () {
         currentLI.parent('.sub-menu').find('li').removeClass('current selected');
         currentLI.addClass('current selected');
     });
+
     jQuery(document).on('click', '.header-aside div.menu-item-has-children > a', function (e) {
         e.preventDefault();
         var current = jQuery(this).closest('div.menu-item-has-children');
@@ -225,20 +223,22 @@ jQuery(document).ready(function () {
 		jQuery("#primary-navbar").prepend("<h3>" + sitename + "</h3>");
     }
     window.onresize = function() {
-        if (jQuery(window).width() < 800 && (jQuery("#universal-subnav #menu-secondary_universal_menu").length || jQuery(".side-panel-menu-container #buddypanel-menu").length) ) {
+      if (jQuery(window).width() < 800 && (jQuery("#universal-subnav #menu-secondary_universal_menu").length ||
+          jQuery(".side-panel-menu-container #buddypanel-menu").length) ) {
             jQuery("#masthead.site-header-custom").nextAll().not("script, style, #universal-subnav").first().css("padding-top", "76px");
             jQuery("#primary-navbar").prepend(jQuery("#menu-secondary_universal_menu"));
-			if(jQuery(".side-panel-menu-container #buddypanel-menu").length) {
-				jQuery("#primary-navbar").prepend(jQuery("#buddypanel-menu"));
-			}
-			jQuery("#primary-navbar").prepend("<h3>" + sitename + "</h3>");
-        } else if (jQuery(window).width() > 800 && (jQuery("#primary-navbar #menu-secondary_universal_menu").length || jQuery("#primary-navbar #buddypanel-menu").length) ) {
-            jQuery("#masthead.site-header-custom").nextAll().not("script, style, #universal-subnav").first().css("padding-top", "121px");
-            jQuery("#universal-subnav").append(jQuery("#menu-secondary_universal_menu"));
-			if(jQuery("#primary-navbar #buddypanel-menu").length) {
-				jQuery(".side-panel-menu-container").append(jQuery("#buddypanel-menu"));
-			}
-			jQuery("#primary-navbar h3").remove();
+        		if(jQuery(".side-panel-menu-container #buddypanel-menu").length) {
+        			jQuery("#primary-navbar").prepend(jQuery("#buddypanel-menu"));
+        		}
+            jQuery("#primary-navbar").prepend("<h3>" + sitename + "</h3>");
+      } else if (jQuery(window).width() > 800 && (jQuery("#primary-navbar #menu-secondary_universal_menu").length ||
+                 jQuery("#primary-navbar #buddypanel-menu").length) ) {
+          jQuery("#masthead.site-header-custom").nextAll().not("script, style, #universal-subnav").first().css("padding-top", "121px");
+          jQuery("#universal-subnav").append(jQuery("#menu-secondary_universal_menu"));
+    			if(jQuery("#primary-navbar #buddypanel-menu").length) {
+    				jQuery(".side-panel-menu-container").append(jQuery("#buddypanel-menu"));
+    			}
+    			jQuery("#primary-navbar h3").remove();
         }
         if (jQuery(window).width() > 800) {
             navListOrder();
@@ -255,16 +255,19 @@ jQuery(document).ready(function () {
             jQuery("#primary-menu").append(jQuery(this));
         });
     });
+
     jQuery(document).on('click', '.close-mobile', function (e) {
         e.preventDefault();
         jQuery("body").removeClass("mobile-nav");
         jQuery("#site-navigation-custom.main-navigation").removeClass("show");
         jQuery(".menu-item-has-children").removeClass("show-submenu");
     });
+
     jQuery(document).on('click', '.top-menu.menu-item-has-children', function (e) {
         jQuery('.top-menu.menu-item-has-children').not(this).removeClass("show-submenu");
         jQuery(this).toggleClass("show-submenu");
     });
+
     jQuery('.bottom-menu.menu-item-has-children').on('click', function (e) {
         e.stopPropagation();
         jQuery('.bottom-menu.menu-item-has-children').not(this).removeClass("show-submenu");
@@ -275,105 +278,40 @@ jQuery(document).ready(function () {
 
     // still got to get our credit from make projects
     var site = window.location.hostname.split(".")[0];
-	// add utm parameters to all makeprojects links that don't have them
-	jQuery('[href*="makeprojects.com"]').each(function() {
-		var href = jQuery(this).attr('href');
-		if (href && href.indexOf('utm') == -1) {
-			var medium = site == 'makezine' ? 'blog' : 'make';
-			if(jQuery(this).parents('header').length) {
-				medium = site + "_nav";
-			}
-			href += (href.match(/\?/) ? '&' : '?') + 'utm_source=make&utm_medium=' + medium + '&utm_campaign=' + site + '&utm_content=link';
-			jQuery(this).attr('href', href);
-		}
-	});
 
-	// add profile links to makehub sites
-	if(jQuery("body").hasClass("buddyboss-theme")) {
-		var profilehtml = '<ul id="header-my-account-menu" class="bb-my-account-menu has-icon">' +
-                        '<li class="menu-item icon-added">' +
-                            '<a href="https://make.co/dashboard">' +
-                                '<i class="_mi _before buddyboss bb-icon-board-list" aria-hidden="true"></i><span>My Dashboard</span>' +
-                            '</a>' +
-                        '</li>' +
-                        '<li class="menu-item menu-item-facilitator-portal">' +
-                            '<a href="https://make.co/edit-submission/">' +
-                                '<i class="_mi _before buddyboss bb-icon-graduation-cap" aria-hidden="true"></i><span>Facilitator Portal</span>' +
-                            '</a>' +
-                        '</li>' +
-                        '<li class="menu-item menu-item-event-cart">' +
-                            '<a href="https://make.co/registration-checkout/?event_cart=view#checkout">' +
-                                '<i class="_mi _before buddyboss bb-icon-shopping-cart" aria-hidden="true"></i><span>Event Cart</span>' +
-                            '</a>' +
-                        '</li>' +
-                        '<li class="bp-menu bp-profile-nav menu-item menu-item-has-children">' +
-                            '<a href="https://make.co/members/me/profile/">' +
-                                '<i class="_mi _before buddyboss bb-icon-user-alt" aria-hidden="true"></i><span>Profile</span>' +
-                            '</a>' +
-                            '<div class="wrapper ab-submenu">' +
-                                '<ul class="bb-sub-menu">' +
-                                    '<li class="bp-menu bp-public-sub-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/members/me/profile/">View</a>' +
-                                    '</li>' +
-                                    '<li class="bp-menu bp-edit-sub-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/members/me/profile/edit/">Edit</a>' +
-                                    '</li>' +
-                                    '<li class="bp-menu bp-change-avatar-sub-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/members/me/profile/change-avatar/">Profile Photo</a>' +
-                                    '</li>' +
-                                    '<li class="bp-menu bp-change-cover-image-sub-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/members/me/profile/change-cover-image/">Cover Photo</a>' +
-                                    '</li>' +
-                                '</ul>' +
-                            '</div>' +
-                        '</li>' +
-                        '<li class="bp-menu bp-settings-nav menu-item menu-item-has-children">' +
-                            '<a href="https://make.co/members/me/settings/">' +
-                                '<i class="_mi _before buddyboss bb-icon-settings" aria-hidden="true"></i>' +
-                                '<span>Account</span>' +
-                            '</a>' +
-                            '<div class="wrapper ab-submenu"><ul class="bb-sub-menu">' +
-                                    '<li class="bp-menu bp-settings-notifications-sub-nav menu-item no-icon"><a href="https://make.co/members/me/settings/notifications/">Email Preferences</a></li>' +
-                                    '<li class="bp-menu bp-view-sub-nav menu-item no-icon"><a href="https://make.co/members/me/settings/profile/">Privacy</a></li>' +
-                                    '<li class="bp-menu bp-blocked-members-sub-nav menu-item no-icon"><a href="https://make.co/members/me/settings/blocked-members/">Blocked Members</a></li>' +
-                                    '<li class="bp-menu bp-group-invites-settings-sub-nav menu-item no-icon"><a href="https://make.co/members/me/settings/invites/">Group Invites</a></li>' +
-                                    '<li class="bp-menu bp-export-sub-nav menu-item no-icon"><a href="https://make.co/members/me/settings/export/">Export Data</a></li>' +
-                                '</ul></div>' +
-                        '</li>' +
-                        '<li class="bp-menu bp-friends-nav menu-item menu-item-has-children">' +
-                            '<a href="https://make.co/members/me/friends/">' +
-                                '<i class="_mi _before buddyboss bb-icon-users" aria-hidden="true"></i><span>Connections</span></a>' +
-                            '<div class="wrapper ab-submenu">' +
-                                '<ul class="bb-sub-menu">' +
-                                    '<li class="bp-menu bp-my-friends-sub-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/members/me/friends/">My Connections</a>' +
-                                    '</li>' +
-                                    '<li class="bp-menu bp-requests-sub-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/members/me/friends/requests/">Requests</a>' +
-                                    '</li>' +
-                                '</ul>' +
-                            '</div>' +
-                        '</li>' +
-                        '<li class="bp-menu bp-groups-nav menu-item menu-item-has-children">' +
-                            '<a href="https://make.co/members/me/groups/">' +
-                                '<i class="_mi _before buddyboss bb-icon-groups" aria-hidden="true"></i>' +
-                                '<span>Groups</span>' +
-                            '</a>' +
-                            '<div class="wrapper ab-submenu">' +
-                                '<ul class="bb-sub-menu">' +
-                                    '<li class="bp-menu bp-groups-create-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/groups/create/">Create Group</a>' +
-                                    '</li>' +
-                                    '<li class="bp-menu bp-my-groups-sub-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/members/me/groups/">My Groups</a>' +
-                                    '</li>' +
-                                    '<li class="bp-menu bp-group-invites-sub-nav menu-item no-icon">' +
-                                        '<a href="https://make.co/members/me/groups/invites/">Invitations</a>' +
-                                    '</li>' +
-                                '</ul>' +
-                            '</div>' +
-                        '</li>' +
-                    '</ul>';
-		jQuery("#profileLinks").append(profilehtml);
-	}
+    // add utm parameters to all makeprojects links that don't have them
+    jQuery('[href*="makeprojects.com"]').each(function() {
+    	var href = jQuery(this).attr('href');
+    	if (href && href.indexOf('utm') == -1) {
+    		var medium = site == 'makezine' ? 'blog' : 'make';
+    		if(jQuery(this).parents('header').length) {
+    			medium = site + "_nav";
+    		}
+    		href += (href.match(/\?/) ? '&' : '?') + 'utm_source=make&utm_medium=' + medium + '&utm_campaign=' + site + '&utm_content=link';
+    		jQuery(this).attr('href', href);
+    	}
+    });
+
+//Attention Rio
+  //place this in getProfile function
+  //move this to auth0.js after log in, replace display of avatar and drop down with this
+  // change auth0.js to login to auth0. if not makercamp or makezine, login to wordpress
+  // add profile links to makehub sites
+  //change user@email.com to be the email
+  jQuery.ajax({
+    type: 'GET',
+    url: "/wp-json/MakeHub/v1/userNav/123",
+    timeout: 10000,
+    success: function (data) {
+      $( "#make-login" ).html( data.makeLogin );
+      $( "#make-coins" ).html( data.makeCoins );
+      $( "#make-join" ).html( data.makeJoin );
+    },
+  }).done(function () {
+
+  }).fail(function (xhr, status, error) {
+
+  });
+
+
 });
