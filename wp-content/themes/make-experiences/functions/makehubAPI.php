600e91443b2fc5dc4404bacc50c91b0624e89c93
Rio Roth-Barreiro <rio@make.co>
2022-05-03 12:37:49 -0700

    change Join to Subscribe in universal nav and change the coins to only return 2 decimal places

--- wp-content/themes/make-experiences/functions/makehubAPI.php
+++ wp-content/themes/make-experiences/functions/makehubAPI.php
@@ -165,7 +165,7 @@ function get_make_coins($user_email){
   $coins = '';
   if(!empty($rimarkResp->data)){
     $coins = $rimarkResp->data[0]->attributes->make_wallet->data->attributes->total_supply_owned;
-    $coins = number_format($coins,4); //format with a thousand separators and up to 4 decimal places
+    $coins = number_format($coins24); //format with a thousand separators and up to 2 decimal places
   }
 
   return $coins;
