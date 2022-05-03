caf01afc171b92c54e451a5f3224dd94d519f917
Alicia Williams <alicia@make.co>
2022-05-03 11:01:43 -0600

    new script and functions to process the omeda file and write it to active campaign

---
+++ wp-content/themes/make-experiences/devScripts/OmedaToAcAPI.php
@@ -0,0 +1,86 @@
+<?php // ?>
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+    include '../../../../wp-load.php';
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    //set the headers and URL for the active campaign call
+    $headers = array('Api-Token: 6a90725830fc5e03e6cddebdbc550ee624b30aea80abcaf9d1c239ea6ffeb30ea2f86075');
+    $url = "https://make.api-us1.com/api/3/import/bulk_import";
+
+    //pull the Omeda file
+    /* Omeda file layout -
+        0 - Postal Address Id     1 - First Name    2 - Last Name   3 - Email Address
+        4 - Company Name          5 - Street 1      6 - Street 2    7 - City
+        8 - State                 9 - Zip Code_Postal Code  10 - Country
+        11 - Rollup Expire Date   12 - Promo Code           13 - Order Date
+        14 - Requested Version    15 - Class Description    16 - Payment Status Description
+    */
+    $file = fopen("uploads/make-active-customers.csv", 'r');
+    fgetcsv($file); //skip the header row
+
+    $omedaData = array();
+    $count = 1;
+    while (($line = fgetcsv($file)) !== FALSE) {
+      //build the contacts
+      $contacts[] = array(
+        "email" => $line[3],
+        "first_name" => $line[1],
+        "last_name" => $line[2],
+        "tags"    => array("Omeda Subscriber"),
+        "fields"  => array(
+          "id" => 1,    "value" => $line[5], //address
+          "id" => 162,  "value" => $line[6], //address 2
+          "id" => 2,    "value" => $line[7], //city
+          "id" => 3,    "value" => $line[8], //state
+          "id" => 4,    "value" => $line[9], //Zip Code
+          "id" => 5,    "value" => $line[10], //Country
+          "id" => 156,  "value" => $line[11], //rollup expire date
+          "id" => 157,  "value" => $line[12], //promo code
+          "id" => 158,  "value" => $line[13], //order date
+          "id" => 159,  "value"  => $line[14], //Requested Version
+          "id" => 160,  "value"  => $line[15], //Class Description
+          "id" => 161,  "value"  => $line[16], //Payment Status Description
+        ),
+        "subscribe"  => array(
+          "listid" => 8, //make community
+          "listid" => 18 //make magazine
+        ),
+      );
+    }
+    fclose($file);
+
+    omeda_log('Writing '.count($contacts).' Omeda contacts to Omeda');
+
+    //Omeda API can only handle 250 contacts at a time.
+    $contactOut = array_chunk($contacts,250);
+
+    foreach($contactOut as $contact_out){
+      //each call to AC should call makehub when done to report results
+      $body = array('contacts'=>$contact_out,
+        "callback"=> array(
+          "url" => "https://make.co/wp-json/makehub/v1/AcToMake",
+          "requestType" => "POST",
+          "detailed_results" => "true"
+        )
+      );
+      
+      //send api request here
+      $response = json_decode(postCurl($url, $headers, json_encode($body)));
+
+      //results output here
+      omeda_log('File sent to Active Campaign. Status of send: '.$response->Success.
+      '. # of queued contacts: '.$response->queued_contacts.
+      '. batchId: '.$response->batchId.
+      '. message: '.$response->message);
+    }
+    ?>
+  </body>
+</html>
---
+++ wp-content/themes/make-experiences/functions/OmedaToAcAPI.php
@@ -0,0 +1,41 @@
+<?php
+//This will process the response from AC after they have processed our daily upload file
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'makehub/v1', '/AcToMake', array(
+    'methods' => 'POST',
+    'callback' => 'AC_callback',
+  ) );
+} );
+
+function AC_callback( WP_REST_Request $request ) {
+  //access returned data
+  $success = $request['Success'];
+  $failures = $request['Failures'];
+  $failure_reasons = $request['failure_reasons'];
+
+  //if there were any failures, write them to the log
+  if($failures != 0){
+    // call to function
+    omeda_log('Error in processing the Omeda to Active Campaign feed. There were '.$success.' successfull requests and '.$failures.' failed requests. Please see errors below:');
+    foreach($failure_reasons as $failure_reason){
+      omeda_log($failure_reason);
+    }    
+  }
+
+}
+
+//create a log file
+function omeda_log($log_msg) {
+    $log_filename = "ACtoMake_log";
+    if (!file_exists($log_filename)){
+        // create directory/folder uploads.
+        mkdir($log_filename, 0777, true);
+    }
+
+    $log_file_data = $log_filename . '.log';
+
+    // add message to the log file prepended by todays date and time
+    file_put_contents($log_file_data, date('[m/d/Y H-i-s e] ').$log_msg . "\n", FILE_APPEND);
+}
+
+?>
