cca8430601c06a0aee8b559d23995f0f10ead218
Rio Roth-Barreiro <rio@make.co>
2022-05-04 14:43:23 -0700

    update for project library if it's not working with ajax

--- wp-content/themes/make-makercamp/js/site/buddyboss.js
+++ wp-content/themes/make-makercamp/js/site/buddyboss.js
@@ -1,9 +1,13 @@
 jQuery(window).load(function(){
 	if (window.location.href.indexOf("/project-library/") > -1) {
 		jQuery("article").before("<div class='filter-reset'><a href='/projects-search/' class='btn universal-btn' style='display:none;'>Reset Filters</a></div>");
+		if(document.location.search.length) {
+			jQuery(".filter-reset .btn").css("display", "inline-flex");
+		} else {
+			// if the form changes, show the reset button
+			jQuery(".searchandfilter").change(function(){
+				jQuery(".filter-reset .btn").css("display", "inline-flex");
+			});
+		}
 	}
-	// if the form changes, show the reset button
-	jQuery(".searchandfilter").change(function(){
-		jQuery(".filter-reset .btn").css("display", "inline-flex");
-	});
 });
