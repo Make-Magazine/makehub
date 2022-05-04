62755d545b1ba6eea5bed158a10af373595c40ac
Rio Roth-Barreiro <rio@make.co>
2022-05-04 14:27:43 -0700

    bust cache

--- wp-content/themes/make-makercamp/js/site/buddyboss.js
+++ wp-content/themes/make-makercamp/js/site/buddyboss.js
@@ -1,5 +1,5 @@
 jQuery(window).load(function(){
-	if (window.location.href.indexOf("/projects-search/") > -1) {
+	if (window.location.href.indexOf("/project-library/") > -1) {
 		jQuery("article").before("<div class='filter-reset'><a href='/projects-search/' class='btn universal-btn' style='display:none;'>Reset Filters</a></div>");
 	}
 	// if the form changes, show the reset button
