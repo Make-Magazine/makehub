d2cc80e6be2680ec62d4221234d031145c72f588
Alicia Williams <alicia@make.co>
2022-05-05 11:04:07 -0600

    hardcoding jwt

--- wp-content/themes/make-experiences/functions/makehubAPI.php
+++ wp-content/themes/make-experiences/functions/makehubAPI.php
@@ -44,7 +44,7 @@ function make_user_info( $data ) {
     $headers = setMemPressHeaders();
     $memberInfo = basicCurl("https://make.co/wp-json/mp/v1/members/".$user->ID, $headers);
     $memberArray = json_decode($memberInfo);
-	$membershipButton = "<a href='https://make.co/join/' class='btn membership-btn'>Join Now!</a>";
+	  $membershipButton = "<a href='https://make.co/join/' class='btn membership-btn'>Join Now!</a>";
     if(isset($memberArray->active_memberships) &&
            is_array($memberArray->active_memberships)
         && !empty($memberArray->active_memberships)){
@@ -55,7 +55,7 @@ function make_user_info( $data ) {
         //Premium Membership
         $banner_text = "Premium Member";
         $banner = "https://make.co/wp-content/universal-assets/v1/images/premium-banner.png";
-		$membershipButton = "";
+		    $membershipButton = "";
       }else{
         //free membership, upgrade now
         $banner_text = "Upgrade Membership";
@@ -143,7 +143,11 @@ function make_user_info( $data ) {
 }
 
 //Rimark
-function get_make_coins($user_email){
+function get_make_coins($user_email){	
+	/*
+	Per Elliot in an email datedd 5/5/22 - they are having problems with the JWT generation
+	and we are to use the hard coded one below that expires in 30 days.
+
   //First do the authentication
   $url = "https://devapi.rimark.io/api/auth/local";
 
@@ -154,9 +158,10 @@ function get_make_coins($user_email){
   $authRes = json_decode($authRes);
 
   //returns jwt key
-  $jwt = $authRes->jwt;
+  $jwt = $authRes->jwt;*/
+	$jwt = "d235e4ae83b20485e600769f5df678dcfedb0fbcf61a97d96b361c9ba753f84bbba56c54f8d10957acd4b43759d85eabfdcc24b14dbec8fe20a3f42e36de82e13c9604709ce043188ca0ed6919c9691f729ac14a3d2caa7eda6278b36bcca5f7027bc91bc8b10857b997b613bd157cdc26dd1a4920764ed88588767640e472ed";
 
-  //now let's see how many coins they have
+	//now let's see how many coins they have
   $url = 'https://devapi.rimark.io/api/makes?filters[user_email][$eq]='.$user_email;
   $headers = array("authorization: Bearer ".$jwt);
   $rimarkResp = json_decode(basicCurl($url, $headers));
