be7446dd197f5e4155cab617d953adbe7a7aa540
Alicia Williams <alicia@make.co>
2022-05-19 15:34:48 -0600

    update devscript to update all users one time on auth0

--- wp-content/themes/make-experiences/devScripts/auth0UpdateUser.php
+++ wp-content/themes/make-experiences/devScripts/auth0UpdateUser.php
@@ -11,36 +11,77 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
-//call Auth0 to get authorization token
-$post_data = array("client_id" => AUTH0_CLIENTID, "client_secret" => AUTH0_SECRET, "audience" => "https://makermedia.auth0.com/api/v2/", "grant_type" => "client_credentials");
-$response = postCurl("https://makermedia.auth0.com/oauth/token", array("content-type: application/json"), json_encode($post_data));
+    $args = array('meta_key'=>'wp_auth0_id', 'meta_value'   => '', 'meta_compare' => '!=',  'offset'=>'0','number'=>'1000');
 
-// the response has our token for update metadata
-$json_response = json_decode($response);
+    $allusers = get_users($args);
+    echo 'number of users found '.count($allusers);
 
-echo 'try update user<br/>';
-//update user
-$url = "https://makermedia.auth0.com/api/v2/users/auth0|Make-Community|2";
-echo 'calling '.$url.'<br/>';
+    $userToUpdate = array();
+    foreach($allusers as $user){
+      //get user first and last name
+      $last_name = get_user_meta( $user->ID, 'last_name', true );
+      $first_name = get_user_meta( $user->ID, 'first_name', true );
 
-$access_token = $json_response->access_token;
+      //get user avatar
+      $user_image =
+       bp_core_fetch_avatar (
+           array(  'item_id' => $user->ID, // id of user for desired avatar
+                   'type'    => 'full',
+                   'html'   => FALSE     // FALSE = return url, TRUE (default) = return img html
+           )
+       );
 
-//call Auth0 to get update user information
-$post_data = array("user_metadata" =>
-                    array("first_name" => "Makey",
-                          "last_name" => "Robot",
-                          "picture" => "https://www.makehub.local/wp-content/uploads/avatars/2/5e8cd4bac4027-bpthumb.png",
-                    )
-                  );
-$headers = array("authorization: Bearer ".$access_token, "content-type: application/json");
+       //get user membership level
+       $membershipType = checkMakeCoMems($user);
+       $userToUpdate[] = array(
+         "email" => $user->user_email,
+         "user_metadata" =>
+                     array("first_name" => $first_name,
+                           "last_name" => $last_name,
+                           "picture" => $user_image,
+                           "user_memlevel" => $membershipType
+                     )
+                   );
 
-echo '$post_data<br/>';
-var_dump(json_encode($post_data));
-echo '<br/>';
-$authRes = postCurl($url, $headers, json_encode($post_data),"PATCH");
-echo 'authRes =<br/>';
-var_dump($authRes);
-echo '<br/>';
+
+
+    }
+
+    die('stop here');
+
+    //call Auth0 to get authorization token
+    $post_data = array("client_id" => AUTH0_CLIENTID, "client_secret" => AUTH0_SECRET, "audience" => "https://makermedia.auth0.com/api/v2/", "grant_type" => "client_credentials");
+    $response = postCurl("https://makermedia.auth0.com/oauth/token", array("content-type: application/json"), json_encode($post_data));
+
+    // the response has our token for update metadata
+    $json_response = json_decode($response);
+
+    //update user
+
+    echo 'calling '.$url.'<br/>';
+
+    $access_token = $json_response->access_token;
+    $headers = array("authorization: Bearer ".$access_token, "content-type: application/json");
+
+    foreach($userToUpdate as $user_id=>$auth0ID){
+
+      $url = "https://makermedia.auth0.com/api/v2/users/".$auth0ID;
+      //call Auth0 to get update user information
+      $post_data = array("user_metadata" =>
+                      array("first_name" => "Makey",
+                            "last_name" => "Robot",
+                            "picture" => "https://www.makehub.local/wp-content/uploads/avatars/2/5e8cd4bac4027-bpthumb.png",
+                            "user_memlevel" => $membershipType
+                      )
+                    );
+      $authRes = postCurl($url, $headers, json_encode($post_data),"PATCH");
+
+      //if there is an error, display here
+      echo 'authRes =<br/>';
+      var_dump($authRes);
+      echo '<br/>';
+    }
+
 ?>
 </body>
 </html>
