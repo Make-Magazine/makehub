70b2af28223faa70208a66e07a7a1bc11ba9f975
Alicia Williams <alicia@make.co>
2022-05-23 20:20:57 -0600

    for ajax return the thumb avatar which should be 150x150

--- wp-content/universal-assets/v1/universal-functions.php
+++ wp-content/universal-assets/v1/universal-functions.php
@@ -290,7 +290,7 @@ function set_ajax_params(){
   $user_image =
         bp_core_fetch_avatar (
             array(  'item_id' => $user->ID, // id of user for desired avatar
-                    'type'    => 'full',
+                    'type'    => 'thumb',
                     'html'   => FALSE     // FALSE = return url, TRUE (default) = return img html
             )
         );
