c61265be396e6720437d8c591625fb30bac5b616
Rio Roth-Barreiro <rio@make.co>
2022-05-25 12:09:26 -0700

    add user projects to wordpress default search and fix the reset filter link

--- wp-content/themes/make-makercamp/js/site/project-library.js
+++ wp-content/themes/make-makercamp/js/site/project-library.js
@@ -1,6 +1,6 @@
 jQuery(window).load(function(){
 	if (window.location.href.indexOf("/project-library/") > -1) {
-		jQuery("article").before("<div class='filter-reset'><a href='/projects-search/' class='btn universal-btn' style='display:none;'>Reset Filters</a></div>");
+		jQuery("article").before("<div class='filter-reset'><a href='/project-library/' class='btn universal-btn' style='display:none;'>Reset Filters</a></div>");
 		if(document.location.search.length) {
 			jQuery(".filter-reset .btn").css("display", "inline-flex");
 		} else {
