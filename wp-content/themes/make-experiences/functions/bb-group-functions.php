c950d518e8778f76c4ef931197b08910670ac616
Alicia Williams <alicia@make.co>
2022-06-03 12:28:41 -0600

    comment out group hub logic

--- wp-content/themes/make-experiences/functions/bb-group-functions.php
+++ wp-content/themes/make-experiences/functions/bb-group-functions.php
@@ -24,6 +24,7 @@ function setup_group_nav() {
                 'item_css_id' => 'event-info'
             ));
         }
+        /*
         // custom page
         if( groups_get_groupmeta( $bp->groups->current_group->id, 'landing_hub_blog_id', true ) &&
             groups_get_groupmeta( $bp->groups->current_group->id, 'landing_hub_post_id', true )) {
@@ -38,7 +39,7 @@ function setup_group_nav() {
                     'user_has_access' => $user_access,
                     'item_css_id' => 'group-hub'
                 ));
-        }
+        }*/
     }
 }
 
@@ -51,10 +52,10 @@ function custom_group_default_tabs($default_tab) {
         if (empty($group)) {
             return $default_tab;
         }
-        if( groups_get_groupmeta( $bp->groups->current_group->id, 'landing_hub_blog_id', true ) &&
+        /*if( groups_get_groupmeta( $bp->groups->current_group->id, 'landing_hub_blog_id', true ) &&
             groups_get_groupmeta( $bp->groups->current_group->id, 'landing_hub_post_id', true )) {
             $default_tab = 'group-hub';
-        }
+        }*/
     endif; // end if ( class_exists( 'BP_Group_Extension' ) )
     return $default_tab;
 }
