e7758098e152fa7b8e90e2489d8214d0643ed177
Alicia Williams <alicia@make.co>
2022-06-03 11:59:17 -0600

    combined general_gf_functions.php and gf_general_functions.php
    gf_supplemental_forms.php
    - removed form type requirement

--- wp-content/themes/make-makercamp/functions/gf_general_functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-//change save and continue retention from 30 days to 90 days
-add_filter( 'gform_incomplete_submissions_expiration_days', 'gwp_days', 1, 10 );
-function gwp_days( $expiration_days ) {
-    // change this value
-    $expiration_days = 90;
-    return $expiration_days;
-}
