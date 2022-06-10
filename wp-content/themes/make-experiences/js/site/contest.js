0700f4ff9b05f0e4845ebc53947b9d5025fac1ba
Rio Roth-Barreiro <rio@make.co>
2022-06-10 09:54:57 -0700

    hide spinner

--- wp-content/themes/make-experiences/js/site/contest.js
+++ wp-content/themes/make-experiences/js/site/contest.js
@@ -4,7 +4,12 @@ jQuery(document).ready(function () {
 		var maxHeight = (jQuery(".wpvc-profile").outerHeight() > 900) ? jQuery(".wpvc-profile").outerHeight() : 900;
 		jQuery('.wpvc-main-wrapper').css("max-height", maxHeight);
 		jQuery(".collapsed").on("click", function(){
-			jQuery('.wpvc-main-wrapper').css("max-height", 1500);
-		});
+			jQuery('.wpvc-main-wrapper').toggleClass(function() {
+				if ( jQuery('.wpvc-main-wrapper').outerHeight > jQuery(".wpvc-profile").outerHeight() ) {
+					return "tallboy";
+				} else {
+					return "short";
+				}
+			});
     }
 });
