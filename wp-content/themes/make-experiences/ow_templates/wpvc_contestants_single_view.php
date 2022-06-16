68d34f66049e16e7ce053bbc535ee005d69d9746
Rio Roth-Barreiro <rio@make.co>
2022-06-15 17:05:17 -0700

    change vote button on contest entries when not logged in to point to the Contest membership sign up

--- wp-content/themes/make-experiences/ow_templates/wpvc_contestants_single_view.php
+++ wp-content/themes/make-experiences/ow_templates/wpvc_contestants_single_view.php
@@ -81,7 +81,7 @@
 							</div>
 							<div class="wpvc-vote">
 								<div id="wpvc-singlecustom-header" data-shortcode="singlecontestantcustom" data-url="<?php echo site_url();?>" data-args='<?php echo $show_args; ?>' data-postid="<?php echo $post->ID; ?>" ></div>
-								<a href="/wp-login.php?redirect_to=<?php echo get_permalink(); ?>" class="btn universal-btn lgn-btn">Please Log In to Vote</a>
+								<a href="/register/contest/" class="btn universal-btn lgn-btn">Vote</a>
 							</div>
 							<div class="wpvc-details">
 								<p><?php echo $short_desc; ?></p>
