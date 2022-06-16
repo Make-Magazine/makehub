c17774f14c15ea8bfb09008eae74fdcaa339729d
Alicia Williams <alicia@make.co>
2022-06-15 19:11:35 -0600

    fix typo

--- wp-content/universal-assets/v1/universal-functions.php
+++ wp-content/universal-assets/v1/universal-functions.php
@@ -363,8 +363,8 @@ function wpb_new_gravatar ($avatar_defaults) {
 add_filter( 'gform_secure_file_download_location', '__return_false' );
 
 //add new Notification event of - send confirmation letter and maker cancelled exhibit
-add_filter( 'gform_notification_events', 'add_event' );
-function add_event( $notification_events ) {    
+add_filter( 'gform_notification_events', 'add_events' );
+function add_events( $notification_events ) {    
     $notification_events['manual']                = __( 'Send Manually', 'gravityforms' );
     return $notification_events;
 }
