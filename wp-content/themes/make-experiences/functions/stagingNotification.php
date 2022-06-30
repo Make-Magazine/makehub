8abf4862b949d358e347b239a6b00abbb98e77ba
Rio Roth-Barreiro <rio@make.co>
2022-06-30 15:31:48 -0700

    staging notification redirects weren't changed when devmakehub was updated from wpengine address

--- wp-content/themes/make-experiences/functions/stagingNotification.php
+++ wp-content/themes/make-experiences/functions/stagingNotification.php
@@ -7,7 +7,7 @@ function change_email_for_wp($email) {
     $homeurl = get_home_url();
 
     //check for our stage, dev or local sites
-    if (strpos($homeurl, 'wpengine.com') !== false || strpos($homeurl, 'makehub.local') !== false || strpos($homeurl, 'makehub.test') !== false) {
+    if (strpos($homeurl, 'devmakehub') !== false || strpos($homeurl, 'wpengine.com') !== false || strpos($homeurl, 'makehub.local') !== false || strpos($homeurl, 'makehub.test') !== false) {
         $email['headers']  = "Content-Type: text/html; charset=ISO-8859-1";
         $email['subject']  = 'Redirect Email sent to ' . $email['to'] . ' - ' . $email['subject'];
         $email['message'] .= '<br />This email is redirected from: '.$homeurl;
