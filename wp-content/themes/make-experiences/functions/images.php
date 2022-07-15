e9058e6d8af317c6f47878abdcd254fd183fd051
Rio Roth-Barreiro <rio@make.co>
2022-07-14 17:01:42 -0700

    bust cache

--- wp-content/themes/make-experiences/functions/images.php
+++ wp-content/themes/make-experiences/functions/images.php
@@ -1,7 +1,7 @@
 <?php
 // Jetpack webp exceptions
 function jetpack_exceptions() {
-  if ( is_page( 11626, 2625 ) ) {
+  if ( is_page( array(11626, 2625) ) ) {
     add_filter( 'jetpack_photon_skip_image', '__return_true' );
   }
 }
