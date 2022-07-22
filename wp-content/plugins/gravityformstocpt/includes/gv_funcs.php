5fc404eb47ab5ea9e51036081bd4d3a42972db87
Alicia Williams <alicia@make.co>
2022-07-21 20:03:48 -0600

    need to set this variable

--- wp-content/plugins/gravityformstocpt/includes/gv_funcs.php
+++ wp-content/plugins/gravityformstocpt/includes/gv_funcs.php
@@ -55,7 +55,8 @@ function gv_status_change($entry_id, $statusToUpdate) {
         $post_id = absint( rgar( $post_info, 'post_id' ) ); // Get post ID.
 
         //if this is an update, we need to update the associated post
-        if($statusToUpdate == 'statAfterEdit' || $statusToUpdate == 'adminEditNoChange'){
+        if($statusToUpdate == 'statAfterEdit' || $statusToUpdate == 'adminEditNoChange'){                    
+          $form = GFAPI::get_form($entry['form_id']);
           gf_gftocpt()->update_post( $post_id, $feed, $entry, $form );
           if($statusToUpdate = 'adminEditNoChange'){
             $statusToUpdate = 'no-change'; //if this is an admin edit, do not change the status
