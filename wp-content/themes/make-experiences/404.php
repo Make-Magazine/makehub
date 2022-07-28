3cdcf3909c909e5f650b4fb4cfe1b96cc7112698
Rio Roth-Barreiro <rio@make.co>
2022-07-28 12:57:27 -0700

    make 404 pages more customizable with classes

--- wp-content/themes/make-experiences/404.php
+++ wp-content/themes/make-experiences/404.php
@@ -8,8 +8,8 @@ get_header( 'universal' );
 
 ?>
 
-<div class="container">
-	<img src="https://make.co/wp-content/universal-assets/v1/images/makey-404.jpg" alt="404, Sorry Page not found" title="This is perhaps not the page you are looking for." />
+<div class="container container-404">
+	<img class="makey-404" src="https://make.co/wp-content/universal-assets/v1/images/makey-404.jpg" alt="404, Sorry Page not found" title="This is perhaps not the page you are looking for." />
 	<div class="btns-404">
 		<a href="javascript:history.back()">Go Back </a>/
 		<a href="/">Go to Home Page </a>
