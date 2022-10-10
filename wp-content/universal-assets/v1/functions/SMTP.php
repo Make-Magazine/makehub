6c25ba4febf08a7e5458800b8e10866d7d58f2ca
Alicia Williams <alicia@make.co>
2022-10-10 15:51:15 -0600

    modifying SMTP to go to postmark instead of mandrill

--- wp-content/universal-assets/v1/functions/SMTP.php
+++ wp-content/universal-assets/v1/functions/SMTP.php
@@ -5,24 +5,11 @@
 add_action('phpmailer_init', 'send_smtp_email');
 
 function send_smtp_email($phpmailer) {
-  // Define that we are sending with SMTP
-  $phpmailer->isSMTP();
-
-  // The hostname of the mail server
-  $phpmailer->Host = "smtp.mandrillapp.com";
-
-  // Use SMTP authentication (true|false)
-  $phpmailer->SMTPAuth = true;
-
-  // SMTP port number - likely to be 25, 465 or 587
-  $phpmailer->Port = "587";
-
-  // Username to use for SMTP authentication
-  $phpmailer->Username = "webmaster@make.co";
-
-  // Password to use for SMTP authentication
-  $phpmailer->Password = "HY-NqCNFBIi4FO-0O2V7Qw";
-
-  // Encryption system to use - ssl or tls
-  $phpmailer->SMTPSecure = "tls";
+  $phpmailer->isSMTP();  // Define that we are sending with SMTP
+  $phpmailer->Host = "smtp.postmarkapp.com"; // The hostname of the mail server
+  $phpmailer->SMTPAuth = true; // Use SMTP authentication (true|false)
+  $phpmailer->Port = "587"; // SMTP port number - likely to be 25, 465 or 587
+  $phpmailer->Username = "PM-T-outbound-r-KHOGDCP5YkbrjHtMcBVb"; // Username to use for SMTP authentication
+  $phpmailer->Password = "MjnmacHOpUyOQHzcfyN7_jnysKlaCcg7kVkF"; // Password to use for SMTP authentication
+  $phpmailer->SMTPSecure = "tls"; // Encryption system to use - ssl or tls
 }
