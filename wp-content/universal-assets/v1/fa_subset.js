a6be9e56dd80bbfd31c1a8e36310de4e54265d5c
Rio Roth-Barreiro <rio@make.co>
2022-10-13 12:25:47 -0700

    fix italicization of font awesome brands, add "circle-times" icon to the subset

--- wp-content/universal-assets/v1/fa_subset.js
+++ wp-content/universal-assets/v1/fa_subset.js
@@ -1,6 +1,6 @@
 const { fontawesomeSubset } = require("fontawesome-subset");
 fontawesomeSubset({
-    solid: ["check", "search", "chevron-up", "chevron-down", "chevron-right", "chevron-left", "ellipsis-h", "bars", "list", "id-card", "calendar-alt", "calendar", "users", "user", "times", "sign-out-alt", "spinner", "wrench", "clock", "tags", "caret-right", "caret-left", "caret-up", "caret-down", "circle", "minus-circle", "plus", "edit", "link",  "images", "globe", "th-list", "th-large", "envelope", "star"],
+    solid: ["check", "search", "chevron-up", "chevron-down", "chevron-right", "chevron-left", "ellipsis-h", "bars", "list", "id-card", "calendar-alt", "calendar", "users", "user", "times", "sign-out-alt", "spinner", "wrench", "clock", "tags", "caret-right", "caret-left", "caret-up", "caret-down", "circle", "minus-circle", "times-circle", "plus", "edit", "link",  "images", "globe", "th-list", "th-large", "envelope", "star"],
     brands: ["facebook", "twitter", "instagram", "youtube", "tiktok", "pinterest", "twitter-square", "facebook-square", 'linkedin', 'snapchat'],
 },
 'fonts',
