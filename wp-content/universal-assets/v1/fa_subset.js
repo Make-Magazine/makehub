7f5f7e5610ed31f5eb46ddf578b2d371bfad5862
Rio Roth-Barreiro <rio@make.co>
2022-11-30 16:59:06 -0800

    open book doesn't work even though it's in the ttf... use closed book instead

--- wp-content/universal-assets/v1/fa_subset.js
+++ wp-content/universal-assets/v1/fa_subset.js
@@ -1,6 +1,6 @@
 const { fontawesomeSubset } = require("fontawesome-subset");
 fontawesomeSubset({
-    solid: ["check", "search", "chevron-up", "chevron-down", "chevron-right", "chevron-left", "ellipsis-h", "bars", "list", "id-card", "calendar-alt", "calendar", "users", "user", "times", "sign-out-alt", "spinner", "wrench", "clock", "tags", "caret-right", "caret-left", "caret-up", "caret-down", "circle", "minus-circle", "times-circle", "plus", "edit", "link",  "images", "globe", "th-list", "th-large", "envelope", "star", "sort", "angle-double-right", "book-open"],
+    solid: ["check", "search", "chevron-up", "chevron-down", "chevron-right", "chevron-left", "ellipsis-h", "bars", "list", "id-card", "calendar-alt", "calendar", "users", "user", "times", "sign-out-alt", "spinner", "wrench", "clock", "tags", "caret-right", "caret-left", "caret-up", "caret-down", "circle", "minus-circle", "times-circle", "plus", "edit", "link",  "images", "globe", "th-list", "th-large", "envelope", "star", "sort", "angle-double-right", "angle-double-left", "book", "book-open", "book-reader"],
     brands: ["facebook", "twitter", "instagram", "youtube", "tiktok", "pinterest", "twitter-square", "facebook-square", 'linkedin', 'snapchat'],
 },
 'fonts',
