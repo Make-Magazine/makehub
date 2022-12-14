681c3835f3ca11af593f522f467530182c97d9b1
Rio Roth-Barreiro <rio@make.co>
2022-12-14 13:36:27 -0800

    add mastodon font awesome icon

--- wp-content/universal-assets/v1/fa_subset.js
+++ wp-content/universal-assets/v1/fa_subset.js
@@ -1,7 +1,7 @@
 const { fontawesomeSubset } = require("fontawesome-subset");
 fontawesomeSubset({
     solid: ["check", "search", "chevron-up", "chevron-down", "chevron-right", "chevron-left", "ellipsis-h", "bars", "list", "id-card", "calendar-alt", "calendar", "users", "user", "times", "sign-out-alt", "spinner", "wrench", "clock", "tags", "caret-right", "caret-left", "caret-up", "caret-down", "circle", "minus-circle", "times-circle", "plus", "edit", "link",  "images", "globe", "th-list", "th-large", "envelope", "star", "sort", "angle-double-right", "angle-double-left", "book", "book-open", "book-reader"],
-    brands: ["facebook", "twitter", "instagram", "youtube", "tiktok", "pinterest", "twitter-square", "facebook-square", 'linkedin', 'snapchat', 'discord'],
+    brands: ["facebook", "twitter", "instagram", "youtube", "tiktok", "pinterest", "twitter-square", "facebook-square", 'linkedin', 'snapchat', 'discord', 'mastodon'],
 },
 'fonts',
 );
