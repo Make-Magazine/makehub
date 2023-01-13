ca65c92f5ad8f21f8f0994175635a43a1d2bb28c
Rio Roth-Barreiro <rio@make.co>
2023-01-13 10:24:15 -0800

    add pinterest tag to makeco

--- wp-content/themes/make-experiences/header.php
+++ wp-content/themes/make-experiences/header.php
@@ -58,6 +58,23 @@
                 })(window, document, 'script', 'dataLayer', 'GTM-5PRW4M2');</script>
             <!-- End Google Tag Manager -->
 
+			<!-- Pinterest Tag -->
+			<script>
+			!function(e){if(!window.pintrk){window.pintrk = function () {
+			window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+			  n=window.pintrk;n.queue=[],n.version="3.0";var
+			  t=document.createElement("script");t.async=!0,t.src=e;var
+			  r=document.getElementsByTagName("script")[0];
+			  r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+			pintrk('load', '2613138638003', {em: 'webmaster@make.co'});
+			pintrk('page');
+			</script>
+			<noscript>
+			<img height="1" width="1" style="display:none;" alt=""
+			  src="https://ct.pinterest.com/v3/?event=init&tid=2613138638003&pd[em]=01392d484c1786527202fad89aa49be69f7f2cc04b95fee86ce581b81e6e4536&noscript=1" />
+			</noscript>
+			<!-- end Pinterest Tag -->
+
         <?php } // end cookie law if  ?>
 
         <?php wp_head(); ?>
