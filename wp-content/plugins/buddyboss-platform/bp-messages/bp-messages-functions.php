7115a333b56ee99fd12e552160c94b3e27924fb9
Alicia Williams <alicia@make.co>
2023-03-23 11:12:37 -0600

    Revert "fixing long running bb queries by pre running part of the query"
    This reverts commit 676aa0483cda4fbf34e656c218609136671373d9.

--- wp-content/plugins/buddyboss-platform/bp-messages/bp-messages-functions.php
+++ wp-content/plugins/buddyboss-platform/bp-messages/bp-messages-functions.php
@@ -1770,12 +1770,7 @@ function bb_messages_update_unread_count( $sub_query, $r ) {
 	$bp = buddypress();
 
 	if ( false === bp_disable_group_messages() || ! bp_is_active( 'groups' ) ) {
-		
-		//Make 
-		$message_list = $wpdb->get_results( "SELECT DISTINCT message_id from {$bp->messages->table_name_meta} WHERE meta_key = 'group_message_users' AND meta_value = 'all' AND message_id IN ( SELECT DISTINCT message_id FROM {$bp->messages->table_name_meta} WHERE meta_key = 'group_message_type' AND meta_value = 'open' ) " );				
-
-		$message_ids = array_map( 'intval', wp_list_pluck( $message_list, 'message_id' ) );
-		$sub_query = "AND m.id IN ( ".implode(",",$message_ids).")";			
+		$sub_query = "AND m.id IN ( SELECT DISTINCT message_id from {$bp->messages->table_name_meta} WHERE meta_key = 'group_message_users' AND meta_value = 'all' AND message_id IN ( SELECT DISTINCT message_id FROM {$bp->messages->table_name_meta} WHERE meta_key = 'group_message_type' AND meta_value = 'open' ) )";
 	} elseif ( bp_is_active( 'groups' ) ) {
 		// Determine groups of user.
 		$groups = groups_get_groups(
@@ -1794,12 +1789,8 @@ function bb_messages_update_unread_count( $sub_query, $r ) {
 			$group_ids_sql = implode( ',', array_unique( $group_ids ) );
 			$group_ids_sql = "AND ( meta_key = 'group_id' AND meta_value NOT IN ({$group_ids_sql}) )";
 		}
-		
-		//Make 
-		$message_list = $wpdb->get_results( "SELECT DISTINCT message_id from {$bp->messages->table_name_meta} WHERE 1 =1 {$group_ids_sql} AND message_id IN ( SELECT DISTINCT message_id from {$bp->messages->table_name_meta} WHERE meta_key  = 'group_message_users' and meta_value = 'all' AND message_id in ( SELECT DISTINCT message_id from {$bp->messages->table_name_meta} where meta_key  = 'group_message_type' and meta_value = 'open' ) ) " );				
 
-		$message_ids = array_map( 'intval', wp_list_pluck( $message_list, 'message_id' ) );
-		$sub_query = "AND m.id IN ( ".implode(",",$message_ids).")";			
+		$sub_query = "AND m.id IN ( SELECT DISTINCT message_id from {$bp->messages->table_name_meta} WHERE 1 =1 {$group_ids_sql} AND message_id IN ( SELECT DISTINCT message_id from {$bp->messages->table_name_meta} WHERE meta_key  = 'group_message_users' and meta_value = 'all' AND message_id in ( SELECT DISTINCT message_id from {$bp->messages->table_name_meta} where meta_key  = 'group_message_type' and meta_value = 'open' ) ) )";
 	}
 
 	return $sub_query;
