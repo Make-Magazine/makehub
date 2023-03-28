458e180c2bd172502ce164dbb4d34c0b57031271
Alicia Williams <alicia@make.co>
2023-03-28 14:11:25 -0600

    new make-community theme

---
+++ wp-content/themes/make-community/footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+
+?>
+			</div><!-- .container -->
+		</div><!-- #content -->
+
+		<?php    require_once(WP_CONTENT_DIR.'/universal-assets/v1/page-elements/universal-footer.html'); ?>
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+
+	<a href="mailto:feedback@make.co" class="btn feedback-btn">FEEDBACK</a>
+
+	<?php
+    // Tracking pixels users can turn off through the cookie law checkbox -- defaults to yes
+	if(!isset($_COOKIE['cookielawinfo-checkbox-non-necessary']) || $_COOKIE['cookielawinfo-checkbox-non-necessary'] == "yes" ) {
+		?>
+		<!-- Start Active Campaign Pixel -->
+		<script type="text/javascript">
+			(function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+			vgo('setAccount', '1000801328');
+			vgo('setTrackByDefault', true);
+			vgo('process');
+		</script>
+		<!-- Start Active Campaign Pixel -->
+<?php } ?>
+</body>
+</html>
---
+++ wp-content/themes/make-community/functions.php
@@ -0,0 +1,45 @@
+<?php
+remove_action('wp_head', 'wp_generator');
+
+//universal functions
+require_once(ABSPATH . 'wp-content/universal-assets/v2/universal-functions.php');
+
+// parent theme styles - required
+function onecommunity_child_enqueue_styles() {
+	wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css');
+}
+add_action('wp_enqueue_scripts', 'onecommunity_child_enqueue_styles');
+
+
+//parent theme scripts - required
+function onecommunity_js_functions_child() {
+	wp_enqueue_script( 'onecommunity-js-functions-child', get_stylesheet_directory_uri() . '/js/functions.js', true );
+}
+add_action( 'wp_enqueue_scripts', 'onecommunity_js_functions_child' );
+
+// the default wp user created emails are bad, we got auth0 for that
+function disable_new_user_notifications() {
+    remove_action( 'register_new_user', 'wp_send_new_user_notifications' );
+}
+add_action( 'init', 'disable_new_user_notifications' );
+
+// Include all function files in the make-community/functions directory:
+foreach (glob(get_stylesheet_directory() . '/functions/*.php') as $file) {
+    include_once $file;
+}
+/*
+// Include all custom post type files in the make-experiences/cpts directory:
+foreach (glob(get_stylesheet_directory() . '/cpt/*.php') as $file) {
+    include_once $file;
+}
+
+// Include all class files in the make-experiences/classes directory:
+foreach (glob(dirname(__FILE__) . '/classes/*.php') as $file) {
+    include_once $file;
+}
+*/
+
+//include any subfolders like 'widgets'
+//foreach (glob(dirname(__FILE__) . '/classes/*/*.php') as $file) {
+  //  include_once $file;
+//}
