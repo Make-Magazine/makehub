78f72f240333ecb10343213f013f70d2412f591b
Alicia Williams <alicia@make.co>
2023-03-30 13:38:33 -0600

    converting header and footer to the one community layout. moved tracking pixels to it's own function folder

--- wp-content/themes/make-community/footer.php
+++ wp-content/themes/make-community/footer.php
@@ -3,28 +3,25 @@
  * The template for displaying the footer
  */
 ?>
-			</div><!-- .container -->
-		</div><!-- #content -->
-
-		<?php    require_once(WP_CONTENT_DIR.'/universal-assets/v2/page-elements/universal-footer.html'); ?>
-	</div><!-- #page -->
-
-	<?php wp_footer(); ?>
-
-	<a href="mailto:feedback@make.co" class="btn feedback-btn">FEEDBACK</a>
-
-	<?php
-    // Tracking pixels users can turn off through the cookie law checkbox -- defaults to yes
-	if(!isset($_COOKIE['cookielawinfo-checkbox-non-necessary']) || $_COOKIE['cookielawinfo-checkbox-non-necessary'] == "yes" ) {
-		?>
-		<!-- Start Active Campaign Pixel -->
-		<script type="text/javascript">
-			(function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
-			vgo('setAccount', '1000801328');
-			vgo('setTrackByDefault', true);
-			vgo('process');
-		</script>
-		<!-- Start Active Campaign Pixel -->
+<div class="clear"></div>
+<?php
+if ( get_theme_mod( 'onecommunity_sidenav_enable', true ) == true && shortcode_exists( 'onecommunity-sidenav' ) ) {
+	 echo do_shortcode( '[onecommunity-sidenav]' );
+} else { ?>
+	<style type="text/css">
+	body { padding-right:0px!important; }
+	header#main { padding-right: 0px; }
+</style>
 <?php } ?>
+
+
+<?php
+wp_reset_query();
+?>
+
+<?php    require_once(WP_CONTENT_DIR.'/universal-assets/v2/page-elements/universal-footer.html'); ?>
+
+<?php wp_footer(); ?>
+
 </body>
 </html>
