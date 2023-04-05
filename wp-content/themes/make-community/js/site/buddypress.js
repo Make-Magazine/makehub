7cf64a55f2ba3b6e5e177115fbf47438be32505f
Rio Roth-Barreiro <rio@make.co>
2023-04-05 12:34:35 -0700

    add menu options back for dynamic group widget

--- wp-content/themes/make-community/js/site/buddypress.js
+++ wp-content/themes/make-community/js/site/buddypress.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function () {
 	if(jQuery(".widget_bp_groups_widget").length) {
-		jQuery(".widget_bp_groups_widget h2").after("<i class='fa-ellipsis-v menu-button'></i>");
+		jQuery(".widget_bp_groups_widget h2").append("<i class='menu-button'></i>");
+		jQuery(".widget_bp_groups_widget h2 .menu-button").on("click", function(){
+			console.log("here we go");
+			jQuery(this).closest('h2').next(".item-options").toggle();
+		});
 	}
 });
