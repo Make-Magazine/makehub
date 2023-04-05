87f5404ad9ea641953d5ce71a5205f82afb21be3
Rio Roth-Barreiro <rio@make.co>
2023-04-05 11:55:41 -0700

    more widget styling

---
+++ wp-content/themes/make-community/js/site/buddypress.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function () {
+	if(jQuery(".widget_bp_groups_widget").length) {
+		jQuery(".widget_bp_groups_widget h2").after("<i class='fa-ellipsis-v menu-button'></i>");
+	}
+});
