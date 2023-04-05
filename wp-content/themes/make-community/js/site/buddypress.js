d3cb59ddd645b82ca55481ba2d4e08008d254ef1
Rio Roth-Barreiro <rio@make.co>
2023-04-05 12:38:15 -0700

    cache busted

--- wp-content/themes/make-community/js/site/buddypress.js
+++ wp-content/themes/make-community/js/site/buddypress.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function () {
 	if(jQuery(".widget_bp_groups_widget").length) {
 		jQuery(".widget_bp_groups_widget h2").append("<i class='menu-button'></i>");
 		jQuery(".widget_bp_groups_widget h2 .menu-button").on("click", function(){
-			console.log("here we go");
 			jQuery(this).closest('h2').next(".item-options").toggle();
 		});
 	}
