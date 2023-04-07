fa2269cde0ae236677f7f24d79b2e6e67184bacc
Rio Roth-Barreiro <rio@make.co>
2023-04-07 16:38:15 -0700

    personalize user information widget and styling for events widget on the homepage

--- wp-content/themes/make-community/functions/buddypress.php
+++ wp-content/themes/make-community/functions/buddypress.php
@@ -52,3 +52,140 @@ function member_social_extend(){
 	}
 }
 add_filter( 'bp_before_member_header_meta', 'member_social_extend' );
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////// USER PROFILE FIELDS WIDGET ///////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Register and load the widget
+function makeco_load_widget_user_profile_fields() {
+    register_widget( 'makeco_widget_user_profile_fields' );
+}
+add_action( 'widgets_init', 'makeco_load_widget_user_profile_fields' );
+ 
+// Creating the widget 
+class makeco_widget_user_profile_fields extends WP_Widget {
+ 
+    function __construct() {
+        parent::__construct(
+            // Base ID of your widget
+            'makeco_widget_user_profile_fields', 
+            // Widget name will appear in UI
+            __('Make: Community User Profile Fields', 'makeco-shortcodes'), 
+            // Widget description
+            array( 'description' => __( 'Displays user`s profile fields.', 'makeco-shortcodes' ), ) 
+        );
+    }
+
+    // Creating widget front-end
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+
+        // before and after widget arguments are defined by themes
+        echo $args['before_widget'];
+
+        if ( ! empty( $title ) )
+            echo $args['before_title'] . $title . $args['after_title'];
+        ?>
+        <div class="bp-profile-fields">
+        <?php
+            $profile_field = bp_get_member_profile_data( 'field=Description' );
+            if( $profile_field ) { ?>
+            <div class="item biography">
+                <?php echo esc_attr( $profile_field ) ?>
+            </div>
+        <?php } ?>
+            <div class="item bg">
+                <div class="left">Joined:</div>
+                <div class="right">
+                <?php
+                global $bp;
+                $currentuser = get_userdata( $bp->displayed_user->id );
+                $joined = date_i18n("M, Y", strtotime($currentuser->user_registered));
+                echo '' . $joined . '';
+                ?>
+                </div>
+            </div>
+        <?php
+        $profile_field = bp_get_member_profile_data( 'field=Display Name' );
+        if( $profile_field ) { ?>
+            <div class="item bg">
+                <div class="left">First Name:</div>
+                <div class="right"><?php echo esc_attr( $profile_field ) ?></div>
+            </div>
+        <?php } ?>
+        <?php
+        $profile_field = bp_get_member_profile_data( 'field=Last Name' );
+        if( $profile_field ) { ?>
+            <div class="item bg">
+                <div class="left">Last Name:</div>
+                <div class="right"><?php echo esc_attr( $profile_field ) ?></div>
+            </div>
+        <?php } ?>
+        <?php
+        $profile_field = bp_get_member_profile_data( 'field=Country' );
+        if( $profile_field ) { ?>
+            <div class="item bg">
+                <div class="left">Country:</div>
+                <div class="right"><?php echo esc_attr( $profile_field ) ?></div>
+            </div>
+        <?php } ?>
+        <?php
+        $profile_field = bp_get_member_profile_data( 'field=Job Title' );
+        if( $profile_field ) { ?>
+            <div class="item bg">
+                <div class="left">Job Title:</div>
+                <div class="right"><?php echo esc_attr( $profile_field ) ?></div>
+            </div>
+        <?php } ?>
+        <?php
+        $profile_field = bp_get_member_profile_data( 'field=Website' );
+        if( $profile_field ) { ?>
+            <div class="item bg">
+                <div class="left">Website:</div>
+                <div class="right"><?php echo esc_attr( $profile_field ) ?></div>
+            </div>
+        <?php } ?>
+        <?php
+        $profile_field = bp_get_member_profile_data( 'field=Topics' );
+        if( $profile_field ) { ?>
+            <div class="item bg">
+                <div class="left">Interests:</div>
+                <div class="right"><?php echo esc_attr( $profile_field ) ?></div>
+            </div>
+        <?php } ?>
+
+        </div>
+        <?php
+        echo $args['after_widget'];
+    }
+            
+    // Widget Backend 
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'User Information', 'makeco-shortcodes' );
+        }
+        // Widget admin form
+        ?>
+        <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'makeco-shortcodes' ); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
+        <p>
+        <?php _e( 'It can be customized in themes/make-community/functions/buddypress.php file.', 'makeco-shortcodes' ); ?>
+        </p>
+        
+        <?php
+    }
+    
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        return $instance;
+    }
+    
+}
