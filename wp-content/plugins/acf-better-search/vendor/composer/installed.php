50181716334e4d8751440d87e5ccf1b2a82378ec
Alicia Williams <alicia@make.co>
2023-04-12 09:14:13 -0600

    acf plugin update

--- wp-content/plugins/acf-better-search/vendor/composer/installed.php
+++ wp-content/plugins/acf-better-search/vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => 'gbiorczyk/acf-better-search',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
+        'reference' => '8a26995427d1fc5f67222b923193b87210de0891',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '2f622c7730b34ad3c47fb04b2f6305c1bf6bf1ea',
-        'name' => 'gbiorczyk/acf-better-search',
         'dev' => false,
     ),
     'versions' => array(
         'gbiorczyk/acf-better-search' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
+            'reference' => '8a26995427d1fc5f67222b923193b87210de0891',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '2f622c7730b34ad3c47fb04b2f6305c1bf6bf1ea',
             'dev_requirement' => false,
         ),
     ),
