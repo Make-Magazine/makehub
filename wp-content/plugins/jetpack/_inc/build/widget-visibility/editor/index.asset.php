dec2048a3a38ae087be561f99bfcca782a8d2b13
Alicia Williams <alicia@make.co>
2023-04-12 09:14:27 -0600

    jetpack update

--- wp-content/plugins/jetpack/_inc/build/widget-visibility/editor/index.asset.php
+++ wp-content/plugins/jetpack/_inc/build/widget-visibility/editor/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => '24da580760f34023f1e8a7134e7fca9e');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => '6be669d3d5c240e8b336');
