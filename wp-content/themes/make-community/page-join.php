e1674b05f8353c7209adb723ab4596065e0bca45
Alicia Williams <alicia@make.co>
2023-04-21 10:58:22 -0600

    update join page wording for upgrades

--- wp-content/themes/make-community/page-join.php
+++ wp-content/themes/make-community/page-join.php
@@ -21,7 +21,7 @@ while ( have_posts() ) : the_post(); ?>
 								<h1>To access this content, upgrade your membership today!</h1>
 								<h4>Upgrade your subscription for digital Make: Magazine access and exclusive videos. Introductory offer $24.99 the first year.</h4>
 								<a href="/register/premium-subscriber?upgrade=65WSJ3T3GY" class="universal-btn membership-btn upgrade" onclick="pintrk('track', 'addtocart');">Upgrade</a>
-								<div class="disclaimer">** Membership Fees are applied <b>annually</b>. **</div>
+								<div class="disclaimer">** Full membership price ($59.99) applied <b>annually</b> after first year. **</div>
 							<?php } else { ?>
 								<h1>You are already a Member!</h1>
 								<a href="/activity" class="universal-btn-reversed" style="margin:0 auto;width:95%px;display:flex;font-size:24px;min-height:100px;padding:10px 20px;text-transform:capitalize">See what's happening on Make: Community</a>
