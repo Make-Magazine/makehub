7f348f5709fa690b691f4261642203374206a1c0
Rio Roth-Barreiro <rio@make.co>
2023-11-28 14:35:08 -0800

    wrong theme parent for the activity form override

--- wp-content/themes/make-community/buddypress/activity/post-form.php
+++ wp-content/themes/make-community/buddypress/activity/post-form.php
@@ -1,41 +1,31 @@
 <?php
 /**
- * The template for BuddyBoss - Activity Post Form
+ * BuddyPress - Activity Post Form
  *
- * This template can be overridden by copying it to yourtheme/buddypress/activity/post-form.php.
- *
- * @since   BuddyPress 3.1.0
- * @version 1.0.0
+ * @version 3.1.0
  */
 
+?>
+
+<?php
 /*
  * Template tag to prepare the activity post form checks capability and enqueue needed scripts.
  */
 if(current_user_can('mepr-active','rules:11718')){ 
-	bp_nouveau_before_activity_post_form();
+bp_nouveau_before_activity_post_form();
+?>
 
-	$current_action = bp_current_action();
+<h2 class="bp-screen-reader-text"><?php echo esc_html_x( 'Post Update', 'heading', 'buddypress' ); ?></h2>
 
-	if ( bp_is_user_activity() && ! bp_is_activity_tabs_active() && ! empty( $current_action ) ) {
-		$current_action = explode( ',', $current_action );
-	}
+<div id="bp-nouveau-activity-form" class="activity-update-form"></div>
 
-	$bp_hide_class = (
-		! empty( $current_action ) &&
-		! is_array( $current_action ) &&
-		'just-me' !== $current_action &&
-		'activity' !== $current_action
-	) ? 'bp-hide is-bp-hide' : '';
-
-	?>
-
-	<h2 class="bp-screen-reader-text"><?php esc_html_e( 'Post Update', 'buddyboss' ); ?></h2>
-
-	<div id="bp-nouveau-activity-form-placeholder" class="bp-nouveau-activity-form-placeholder-<?php if ( ! bp_is_active( 'media' ) ) { echo ' media-off'; } ?> <?php echo esc_attr( $bp_hide_class ); ?>"></div>
-	<div id="bp-nouveau-activity-form" class="activity-update-form<?php if ( ! bp_is_active( 'media' ) ) { echo ' media-off'; } ?> <?php echo esc_attr( $bp_hide_class ); ?>">blah it's me</div>
-	<?php
-	/*
-	* Template tag to load the Javascript templates of the Post form UI.
-	*/
-	bp_nouveau_after_activity_post_form();
-}
+<?php
+/*
+ * Template tag to load the Javascript templates of the Post form UI.
+ */
+bp_nouveau_after_activity_post_form();
+?>
+<div class="clear"></div>
+<?php 
+} 
+?>
