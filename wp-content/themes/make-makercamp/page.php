9060535a1a8ec0dabb1e15c5670e0f2ec89af9ae
Rio Roth-Barreiro <rio@make.co>
2023-12-20 15:15:11 -0800

    makercamp pages need content!

--- wp-content/themes/make-makercamp/page.php
+++ wp-content/themes/make-makercamp/page.php
@@ -20,7 +20,8 @@ get_header();
         if (have_posts()) :
 
             while (have_posts()) :
-                the_post();              
+                the_post();    
+                the_content();          
                 // Are there custom panels to display?
                 if (have_rows('content_panels')) {
                     // loop through the rows of data      
