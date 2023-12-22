f0cd6dd31222ef1a152b3a62aa60abc9c2cc3c82
Rio Roth-Barreiro <rio@make.co>
2023-12-22 10:15:48 -0800

    add a close button to all shopify popouts that are missing it

--- wp-content/themes/make-makercamp/js/site/shopify.js
+++ wp-content/themes/make-makercamp/js/site/shopify.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function () {
+	if(jQuery('[id^=product-component]').find('.shopify-close-btn').length == 0) {
+		jQuery('[id^=product-component]').append('<i class="shopify-close-btn fas fa-angle-double-right"></i>');
+	}	
 	jQuery(".shopify-close-btn").on("click", function(){
 		jQuery(".shopify-buy-frame").toggleClass("minimize");
 	});
