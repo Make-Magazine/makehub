209841989431244f0d284bddd636da4a52145f10
Alicia Williams <alicia@make.co>
2024-02-22 11:13:30 -0700

    change to add clarity text

--- wp-content/themes/make-community/buddypress/members/single/settings/general.php
+++ wp-content/themes/make-community/buddypress/members/single/settings/general.php
@@ -12,7 +12,7 @@ bp_nouveau_member_hook( 'before', 'settings_template' );
 	<h2 class="screen-heading email-settings-screen">
 		<?php _e( 'Change Password', 'buddypress' ); ?>
 	</h2>
-
+	<p>By clicking the below button, a link will be emailed to you to reset your password</p>
 	<form action="<?php echo esc_url( bp_displayed_user_domain() . bp_nouveau_get_component_slug( 'settings' ) . '/general' ); ?>" method="post" class="standard-form" id="your-profile">
 		<input type="hidden" name="email" id="email" value="<?php echo esc_attr( bp_get_displayed_user_email() ); ?>" class="settings-input" <?php bp_form_field_attributes( 'email' ); ?>/>
 		<input type="password" name="pwd" id="pwd" style="display:none" value="Auth0pass" / >	
